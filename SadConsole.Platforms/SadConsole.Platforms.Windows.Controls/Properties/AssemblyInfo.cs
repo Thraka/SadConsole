47950eefa182d340b11960d1b4a718771ce3c4ef
Ed Ropple <ed@edropple.com>
2015-04-25 21:12:48 -0400

    Added Ansi project

--- SadConsole.Platforms/SadConsole.Platforms.Windows.Controls/Properties/AssemblyInfo.cs
+++ SadConsole.Platforms/SadConsole.Platforms.Windows.Controls/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SadConsole.Ansi")]
-[assembly: AssemblyDescription("Ansi library for SadConsole")]
+[assembly: AssemblyTitle("SadConsole.Platforms.Windows.Controls")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SadConsole.Ansi")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("SadConsole.Platforms.Windows.Controls")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("87a13c7b-f081-4df3-a5b0-e91f64433794")]
+[assembly: Guid("46f11907-cef3-4069-b041-0a88880981e6")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.111.1")]
-[assembly: AssemblyFileVersion("1.3.111.1")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
--- SadConsole.Platforms/SadConsole.Platforms.Windows.GameHelpers/Properties/AssemblyInfo.cs
+++ SadConsole.Platforms/SadConsole.Platforms.Windows.GameHelpers/Properties/AssemblyInfo.cs
@@ -5,24 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SadConsole.Ansi")]
-[assembly: AssemblyProduct("Ansi library for SadConsole")]
+[assembly: AssemblyTitle("SadConsole.Platforms.Windows.GameHelpers")]
 [assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("SadConsole.Platforms.Windows.GameHelpers")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type. Only Windows
-// assemblies support COM.
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// On Windows, the following GUID is for the ID of the typelib if this
-// project is exposed to COM. On other platforms, it unique identifies the
-// title storage container when deploying this assembly to the device.
-[assembly: Guid("7b327132-1cc9-4287-8441-4c82697fc9b6")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d348c254-ab16-4b48-9f61-2ffee51dfd4a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.111.3")]
-[assembly: AssemblyFileVersionAttribute("1.3.111.3")]
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
