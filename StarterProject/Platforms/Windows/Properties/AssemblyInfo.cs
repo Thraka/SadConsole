35ec7d5d5ac015256bafe8950701b00ca4b1f681
Ed Ropple <ed@edropple.com>
2015-04-26 12:31:55 -0400

    Broke StarterProject out into a shared project and endpoints; have not tested MacOS and Linux yet but they seem to compile

--- StarterProject/Platforms/Windows/Properties/AssemblyInfo.cs
+++ StarterProject/Platforms/Windows/Properties/AssemblyInfo.cs
@@ -5,24 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StarterProject")]
-[assembly: AssemblyProduct("StarterProject")]
+[assembly: AssemblyTitle("StarterProject.Windows")]
 [assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StarterProject")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type. Only Windows
-// assemblies support COM.
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// On Windows, the following GUID is for the ID of the typelib if this
-// project is exposed to COM. On other platforms, it unique identifies the
-// title storage container when deploying this assembly to the device.
-[assembly: Guid("e19b3d87-fda0-4085-92e3-e2ce80610771")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d8d3932c-6257-47aa-b1c5-6b650407fb6b")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.111.3")]
-[assembly: AssemblyFileVersionAttribute("1.3.111.3")]
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
