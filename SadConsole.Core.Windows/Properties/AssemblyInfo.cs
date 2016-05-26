c967b545c54e087db7742098888e48d5b8adf2c1
Andy De George <thraka@outlook.com>
2016-05-26 13:56:06 -0700

    Restore project files from migration.

--- SadConsole.Core.Windows/Properties/AssemblyInfo.cs
+++ SadConsole.Core.Windows/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SadConsole.Core.Windows")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SadConsole.Core")]
+[assembly: AssemblyDescription("A MonoGame library that emulates old-school console and command prompt style graphics.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SadConsole.Core.Windows")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("SadLogic")]
+[assembly: AssemblyProduct("SadConsole.Core")]
+[assembly: AssemblyCopyright("Copyright © 2016 Steve De George JR (Thraka)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9a7432d8-0c8b-4c46-b405-b6d1080e6811")]
+[assembly: Guid("9e8424e8-efd3-4158-b49e-feccd7509a7f")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
