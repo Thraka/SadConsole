671e0651b60f33aca3aa307236938894d7028c7c
Andy De George <thraka@outlook.com>
2016-08-08 18:23:50 -0700

    Final SFML build. Added DesktopGL mono game. Redesigned repo.

---
+++ src/StarterProject/SharedCode/CustomConsoles/RandomScrollingConsole.cs
@@ -0,0 +1,107 @@
+﻿#if MONOGAME
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ColorHelper = Microsoft.Xna.Framework.Color;
+#elif SFML
+using Keys = SFML.Window.Keyboard.Key;
+using Rectangle = SFML.Graphics.IntRect;
+using SFML.Graphics;
+#endif
+
+using SadConsole;
+using SadConsole.Consoles;
+using Console = SadConsole.Consoles.Console;
+
+namespace StarterProject.CustomConsoles
+{
+    class RandomScrollingConsole : Console
+    {
+        private TextSurface mainData;
+        private SurfaceEditor messageData;
+        private bool initialized;
+        private bool initializedStep2;
+        private bool initializedStep3;
+
+        public RandomScrollingConsole() : base(80, 25)
+        {
+            messageData = new SurfaceEditor(new TextSurface(10, 1, Engine.DefaultFont));
+            IsVisible = false;
+
+            
+            KeyboardHandler = (cons, info) =>
+            {
+
+                if (info.IsKeyDown(Keys.Left))
+                    cons.TextSurface.RenderArea = new Rectangle(cons.TextSurface.RenderArea.Left - 1, cons.TextSurface.RenderArea.Top, 80, 25);
+
+                if (info.IsKeyDown(Keys.Right))
+                    cons.TextSurface.RenderArea = new Rectangle(cons.TextSurface.RenderArea.Left + 1, cons.TextSurface.RenderArea.Top, 80, 25);
+
+                if (info.IsKeyDown(Keys.Up))
+                    cons.TextSurface.RenderArea = new Rectangle(cons.TextSurface.RenderArea.Left, cons.TextSurface.RenderArea.Top - 1, 80, 25);
+
+                if (info.IsKeyDown(Keys.Down))
+                    cons.TextSurface.RenderArea = new Rectangle(cons.TextSurface.RenderArea.Left, cons.TextSurface.RenderArea.Top + 1, 80, 25);
+
+                return true;
+            };
+
+        }
+
+        protected override void OnVisibleChanged()
+        {
+            if (IsVisible && !initialized)
+            {
+                // Write to the message layer
+                Print(0, 0, "Generating random console data, please wait...");
+                initialized = true;
+            }
+        }
+
+        public override void Render()
+        {
+            // These 3 render calls are a hack to get the console data generated and display a message to the user
+            // Should add in async calls that let us generate these in the background... That would be cool.
+            if (IsVisible)
+            {
+                if (!initialized)
+                    base.Render();
+
+                else if (!initializedStep2)
+                {
+                    initializedStep2 = true;
+                    base.Render();
+                }
+                else if (!initializedStep3)
+                {
+                    base.Render();
+
+                    // Generate the content
+                    TextSurface = new TextSurface(2000, 2000, Engine.DefaultFont); //500mb ?? why?
+                    //Data = new TextSurface(2000, 2000);
+                    //DataViewport = new Rectangle(0, 0, 80, 25);
+                    TextSurface.RenderArea = new Rectangle(0, 0, 80, 25);
+
+                    // Clear message data and make it transparent so that it acts as a layer
+                    messageData.Fill(Color.White, Color.Transparent, 0, null);
+
+                    // We need to set celldata to the big console data so we can use the FillWithRandom method.
+                    FillWithRandomGarbage();
+                    initializedStep3 = true;
+                }
+
+                else
+                {
+                    // Set message data information about where the viewport is located
+                    //messageData.Print(0, 0, $"{ViewArea.X} , {ViewArea.Y}            ", Color.White, Color.Black);
+
+                    // Create a faux layering system.
+                    base.Render();
+
+                    //Renderer.Render(messageData.TextSurface, new Point(0, 0));
+                }
+            }
+        }
+    }
+}
---
+++ src/StarterProject/SharedCode/CustomConsoles/StaticConsole.cs
@@ -0,0 +1,56 @@
+﻿#if MONOGAME
+using Microsoft.Xna.Framework;
+using ColorHelper = Microsoft.Xna.Framework.Color;
+#elif SFML
+using SFML.Graphics;
+#endif
+
+using System;
+using SadConsole;
+using Console = SadConsole.Consoles.Console;
+
+namespace StarterProject.CustomConsoles
+{
+    /// <summary>
+    /// Demonstrates a console that doesn't accept keyboard input, doesn't show the cursor (both defaults for a console), and just displays some custom text.
+    /// </summary>
+    class StaticConsole: Console
+    {
+        public StaticConsole()
+            : base(80, 25)
+        {
+            // Prints a string using the cellData default foreground and background.
+            Print(2, 1, "Text written with the default foreground and background");
+
+            // Prints a string using the ColoredString class.
+            Print(2, 2, new ColoredString("Text using a colored string", ColorHelper.LightBlue, Color.Transparent));
+
+            // Creates a new ColoredString from an existing string and applies a gradient color to each character.
+            ColoredString colorString = "Text using a colored string gradient".CreateGradient(ColorHelper.DarkGreen, ColorHelper.LightGreen);
+            Print(2, 3, colorString);
+
+            // Appends a new ColoredString to the existing ColoredString with a new color gradient.
+            colorString += " with another gradient applied".CreateGradient(ColorHelper.DarkBlue, ColorHelper.LightBlue);
+            Print(2, 4, colorString);
+
+            // Prints a string, then changes the foreground of a single cell.
+            Print(2, 5, "Text written with defaults, then the foreground changed");
+            SetForeground(17, 5, ColorHelper.Yellow);
+
+            // Prints a string, then changes the background of a single cell.
+            Print(2, 6, "Text written with defaults, then the background changed");
+            SetBackground(17, 6, ColorHelper.DarkGray);
+
+            // Prints a string, then changes the foreground and background of a single cell.
+            Print(2, 7, "Text written with defaults, then the foreground and background changed");
+            SetBackground(17, 7, ColorHelper.White);
+            SetForeground(17, 7, ColorHelper.Black);
+
+            // Prints a string, then changes the effect of the cell to a Blink effect.
+            Print(2, 8, "Text written with defaults, then the blink effect applied");
+            SetEffect(17, 8, new SadConsole.Effects.Blink() { BlinkSpeed = 0.5f });
+
+            IsVisible = false;
+        }
+    }
+}
