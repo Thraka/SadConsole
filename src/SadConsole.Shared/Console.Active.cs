046700e8a1ba8ff00319333f7a50fc8ba1419d11
Steve De George JR <thraka.accounts@outlook.com>
2017-02-11 13:58:06 -0800

    Added code comments to active console stuff. Fixed window full screen shade rendering.

--- src/SadConsole.Shared/Console.Active.cs
+++ src/SadConsole.Shared/Console.Active.cs
@@ -14,13 +14,21 @@ namespace SadConsole
 {
     public partial class Console : SurfaceEditor, IConsole
     {
-        //public static IConsole ActiveConsole;
+        /// <summary>
+        /// Represents the active console handing keyboard input and possibly mouse input.
+        /// </summary>
         public static ActiveConsoleStack ActiveConsoles = new ActiveConsoleStack();
 
+        /// <summary>
+        /// Keeps track of the active consoles used for input by SadConsole.
+        /// </summary>
         public class ActiveConsoleStack
         {
             private IConsole activeConsole;
 
+            /// <summary>
+            /// Gets the current active console.
+            /// </summary>
             public IConsole Console
             {
                 get
@@ -28,6 +36,10 @@ namespace SadConsole
                     return activeConsole;
                 }
             }
+
+            /// <summary>
+            /// The stack of consoles for input processing.
+            /// </summary>
             List<IConsole> consoles;
 
             internal ActiveConsoleStack()
@@ -36,8 +48,19 @@ namespace SadConsole
                 activeConsole = null;
             }
 
-            public void Clear() => consoles.Clear();
+            /// <summary>
+            /// Clears all consoles from the active stack along with the current active console.
+            /// </summary>
+            public void Clear()
+            {
+                consoles.Clear();
+                activeConsole = null;
+            }
 
+            /// <summary>
+            /// Adds another console to active stack, setting it as the active (top most in the stack) console.
+            /// </summary>
+            /// <param name="console"></param>
             public void Push(IConsole console)
             {
                 if (console != activeConsole && console != null)
@@ -47,6 +70,10 @@ namespace SadConsole
                 }
             }
 
+            /// <summary>
+            /// Replaces the top console (active console) with the provided instance. Sets <see cref="Console"/> to this instance.
+            /// </summary>
+            /// <param name="console">The console to make active.</param>
             public void Set(IConsole console)
             {
                 if (activeConsole == console)
@@ -58,6 +85,10 @@ namespace SadConsole
                 Push(console);
             }
             
+            /// <summary>
+            /// Removes the console from the active stack. If the instance is the current active console, the active console is set to the last console in the previous console.
+            /// </summary>
+            /// <param name="console">The console to remove.</param>
             public void Pop(IConsole console)
             {
                 if (console == activeConsole)
--- src/SadConsole.Shared/Renderers/WindowRenderer.cs
+++ src/SadConsole.Shared/Renderers/WindowRenderer.cs
@@ -37,7 +37,7 @@ namespace SadConsole.Renderers
         public override void Render(ISurface surface, bool force = false)
         {
             if (IsModal && ModalTint.A != 0)
-                Global.DrawCalls.Add(new DrawCallColoredRect(Global.RenderRect, ModalTint));
+                Global.DrawCalls.Add(new DrawCallColoredRect(new Rectangle(0, 0, Global.WindowWidth, Global.WindowHeight), ModalTint));
 
             base.Render(surface);
         }
