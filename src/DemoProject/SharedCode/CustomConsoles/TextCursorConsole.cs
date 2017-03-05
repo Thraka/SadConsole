d2d9ab7396af3fbe4e38808b6b0ca73f7a1bc343
Andy De George <thraka@outlook.com>
2017-03-05 00:30:25 -0800

    Added a new sample for drawing a text cursor

---
+++ src/DemoProject/SharedCode/CustomConsoles/TextCursorConsole.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using ColorHelper = Microsoft.Xna.Framework.Color;
+
+using Console = SadConsole.Console;
+using SadConsole;
+using System;
+using System.Linq;
+using SadConsole.Input;
+
+namespace StarterProject.CustomConsoles
+{
+    class TextCursorConsole : Console, IConsoleMetadata
+    {
+        SadConsole.GameHelpers.GameObject mouseCurosr;
+
+        public ConsoleMetadata Metadata
+        {
+            get
+            {
+                return new ConsoleMetadata() { Title = "Text Mouse Cursor", Summary = "Draws a game object where ever the mouse cursor is." };
+            }
+        }
+
+        public TextCursorConsole()
+            : base(80, 23)
+        {
+            mouseCurosr = new SadConsole.GameHelpers.GameObject();
+            mouseCurosr.Animation = new SadConsole.Surfaces.AnimatedSurface("default", 1, 1);
+            mouseCurosr.Animation.CreateFrame();
+            mouseCurosr.Animation.CurrentFrame.Cells[0].Glyph = 178;
+        }
+
+        public override void Draw(TimeSpan delta)
+        {
+            // First draw the console
+            base.Draw(delta);
+
+            mouseCurosr.Draw(delta);
+        }
+
+        public override bool ProcessMouse(MouseConsoleState state)
+        {
+            mouseCurosr.Position = state.WorldPosition;
+
+            return base.ProcessMouse(state);
+        }
+    } 
+}
