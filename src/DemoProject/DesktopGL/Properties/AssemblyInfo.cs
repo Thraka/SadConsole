5113be324362362aad089e8cc832db95a3b89791
Steve De George JR <thraka.accounts@outlook.com>
2018-01-13 12:17:14 -0800

    Fix missing dispose! ack!

--- src/DemoProject/DesktopGL/Properties/AssemblyInfo.cs
+++ src/DemoProject/DesktopGL/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SadConsole Samples DX")]
+[assembly: AssemblyTitle("SadConsole Samples GL")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
