09b709320c72d69d4d1cd6d3332cfa0c9928e654
Andy De George <thraka.accounts@outlook.com>
2018-03-01 13:47:55 -0800

    Initial commit of abandoning the "in game" editor.

---
+++ Editor/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace SadConsole.Editor
+{
+    internal partial class Form1 : Form
+    {
+        int panelSplitterPosition = 0;
+        Control selectedToolPanel;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void UpdateTree()
+        {
+            if (treeView1.SelectedNode != null)
+            {
+                var previousSelectedItem = ((TreeViewNodeScreen)treeView1.SelectedNode).Screen;
+                treeView1.Nodes.Clear();
+                treeView1.Nodes.Add(new TreeViewNodeScreen(SadConsole.Global.CurrentScreen));
+                var newNodeSelected = FindScreenInNodes(previousSelectedItem, treeView1.Nodes);
+
+                if (newNodeSelected == null)
+                    treeView1.SelectedNode = treeView1.Nodes[0];
+                else
+                    treeView1.SelectedNode = newNodeSelected;
+            }
+            else
+            {
+                treeView1.Nodes.Clear();
+                treeView1.Nodes.Add(new TreeViewNodeScreen(SadConsole.Global.CurrentScreen));
+                treeView1.SelectedNode = treeView1.Nodes[0];
+            }
+        }
+
+        private TreeViewNodeScreen FindScreenInNodes(IScreen screen, TreeNodeCollection nodes)
+        {
+            foreach (var item in nodes)
+            {
+                TreeViewNodeScreen castedItem = (TreeViewNodeScreen)item;
+                if (castedItem.Screen == screen)
+                    return castedItem;
+                else
+                {
+                    var result = FindScreenInNodes(screen, castedItem.Nodes);
+
+                    if (result != null)
+                        return result;
+                }
+            }
+
+            return null;
+        }
+        
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            //if (treeView1.SelectedNode == null)
+            //{
+            //    pnlScreenSettings.IsError = true;
+            //}
+            //else
+            //{
+            //    DataContext.Instance.Screen = ((TreeViewNodeScreen)treeView1.SelectedNode).Screen;
+            //    pnlScreenSettings.IsError = DataContext.Instance.IsScreenConsole;
+            //    pnlScreenSettings.RefreshScreen();
+            //}
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //editorBindingSource.DataSource = DataContext.Instance;
+            //DataContext.Instance.SelectedTool = DataContext.Instance.Tools[0];
+            //panelSplitterPosition = splitContainer1.Panel2MinSize;
+            //splitContainer1.Panel2Collapsed = true;
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            UpdateTree();
+        }
+
+        private void chkEditMode_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkEditMode.Checked)
+            {
+                UpdateTree();
+                splitContainer1.Panel2Collapsed = false;
+                splitContainer1.SplitterDistance = splitContainer1.Width - panelSplitterPosition;
+                //DataContext.Instance.EnableEditMode();
+                //DataContext.Instance.Screen = ((TreeViewNodeScreen)treeView1.SelectedNode).Screen;
+            }
+            else
+            {
+                panelSplitterPosition = splitContainer1.Width - splitContainer1.SplitterDistance;
+                SadConsole.Settings.DoUpdate = true;
+                splitContainer1.Panel2Collapsed = true;
+                //DataContext.Instance.DisableEditMode();
+            }
+        }
+
+        private void editorBindingSource_CurrentItemChanged(object sender, EventArgs e)
+        {
+            //if (DataContext.Instance.SelectedToolPanel != null && selectedToolPanel != DataContext.Instance.SelectedToolPanel)
+            //{
+            //    if (selectedToolPanel != null)
+            //        selectedToolPanel.Parent.Controls.Remove(selectedToolPanel);
+
+            //    selectedToolPanel = DataContext.Instance.SelectedToolPanel;
+
+            //    //Controls.Add(selectedToolPanel);
+            //    pnlToolsList.Parent.Controls.Add(selectedToolPanel);
+            //    selectedToolPanel.Location = new Point(pnlToolsList.Left, pnlToolsList.Bottom + pnlToolsList.Margin.Bottom);
+            //    selectedToolPanel.Width = pnlToolsList.Width;
+            //    //selectedToolPanel.Anchor = pnlToolsList.Anchor;
+            //}
+        }
+
+        private void cboToolsList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cboToolsList.DataBindings[0].WriteValue();
+        }
+
+        private class TreeViewNodeScreen : TreeNode
+        {
+            public IScreen Screen;
+            public bool Hide;
+
+            public TreeViewNodeScreen(IScreen screen)
+            {
+                var debugger = screen.GetType().GetTypeInfo().GetCustomAttributes<System.Diagnostics.DebuggerDisplayAttribute>().FirstOrDefault();
+                Hide = debugger.Value.Contains("(hide)");
+
+                if (debugger != null)
+                    Text = debugger.Value;
+                else
+                    Text = screen.ToString();
+
+                Screen = screen;
+
+                foreach (var child in screen.Children)
+                {
+                    var node = new TreeViewNodeScreen(child);
+
+                    if (!node.Hide)
+                        Nodes.Add(node);
+                }
+
+                Expand();
+            }
+        }
+
+        private void btnSettings_Click(object sender, EventArgs e)
+        {
+            //using (var form = new Forms.EditorSettings())
+            //{
+            //    form.ShowDialog(this);
+            //}
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            //DataContext.Instance.PauseEditMode = true;
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            //DataContext.Instance.PauseEditMode = false;
+        }
+    }
+}
---
+++ Editor/Game.cs
@@ -0,0 +1,171 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SadConsole.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SadConsole.Editor
+{
+    public class Game: MonoGame.Forms.Controls.UpdateWindow
+    {
+        GameTime gameTime = new GameTime();
+
+        //protected override void OnClientSizeChanged(EventArgs e)
+        //{
+        //    base.OnClientSizeChanged(e);
+
+        //    if (SwapChainRenderTarget != null)
+        //    {
+        //        Global.OriginalRenderTarget = SwapChainRenderTarget;
+        //        Global.ResetRendering();
+        //    }
+        //}
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            //Global.OriginalRenderTarget = SwapChainRenderTarget;
+
+            //Global.GraphicsDevice = GraphicsDevice;
+            //Global.SpriteBatch = new Microsoft.Xna.Framework.Graphics.SpriteBatch(GraphicsDevice);
+            //Global.FontDefault = Global.LoadFont(SadConsole.Game.WpfFont).GetFont(SadConsole.Font.FontSizes.One);
+
+            //Global.WindowWidth = Global.RenderWidth = (Global.FontDefault.Size.X * SadConsole.Game.WpfConsoleWidth);
+            //Global.WindowHeight = Global.RenderHeight = (Global.FontDefault.Size.Y * SadConsole.Game.WpfConsoleHeight);
+            //Global.ResetRendering();
+
+            //// Tell the main engine we're ready
+            //SadConsole.Game.OnInitialize?.Invoke();
+
+            //// After we've init, clear the graphics device so everything is ready to start
+            //Global.GraphicsDevice.SetRenderTarget(Global.OriginalRenderTarget);
+        }
+
+        protected override void Draw()
+        {
+            base.Draw();
+
+            //if (Settings.DoDraw)
+            //{
+            //    Global.GameTimeRender = gameTime;
+            //    Global.GameTimeElapsedRender = gameTime.ElapsedGameTime.TotalSeconds;
+
+            //    // Clear draw calls for next run
+            //    Global.DrawCalls.Clear();
+
+            //    // Make sure all items in the screen are drawn. (Build a list of draw calls)
+            //    Global.CurrentScreen?.Draw(gameTime.ElapsedGameTime);
+
+            //    // Draw the editor brush on its surface to render later
+            //    if (DataContext.Instance.IsEditMode)
+            //        if (DataContext.Instance.SelectedTool != null)
+            //            DataContext.Instance.SelectedTool.Brush.Draw(gameTime.ElapsedGameTime);
+
+            //    SadConsole.Game.OnDraw?.Invoke(gameTime);
+
+            //    // Render to the global output texture
+            //    GraphicsDevice.SetRenderTarget(Global.RenderOutput);
+
+            //    // Inner clear color
+            //    if (DataContext.Instance.IsEditMode)
+            //    {
+            //        if (Config.Instance.DrawEditClearColorInner)
+            //            GraphicsDevice.Clear(Config.Instance.EditClearColorInner);
+            //        else
+            //            GraphicsDevice.Clear(Settings.ClearColor);
+            //    }
+            //    else
+            //        GraphicsDevice.Clear(Settings.ClearColor);
+
+            //    // Render each draw call
+            //    Global.SpriteBatch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied, SamplerState.PointClamp, DepthStencilState.DepthRead, RasterizerState.CullNone);
+            //    foreach (var call in Global.DrawCalls)
+            //    {
+            //        call.Draw();
+            //    }
+            //    Global.SpriteBatch.End();
+            //    GraphicsDevice.SetRenderTarget(Global.OriginalRenderTarget);
+                
+            //    // If we're going to draw to the screen, do it.
+            //    if (Settings.DoFinalDraw)
+            //    {
+            //        if (DataContext.Instance.IsEditMode)
+            //        {
+            //            if (Config.Instance.DrawEditClearColorOuter)
+            //                GraphicsDevice.Clear(Config.Instance.EditClearColorOuter);
+            //            else
+            //                GraphicsDevice.Clear(Settings.ClearColor);
+            //        }
+            //        else
+            //            GraphicsDevice.Clear(Settings.ClearColor);
+
+
+            //        Editor.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied, SamplerState.PointClamp, DepthStencilState.DepthRead, RasterizerState.CullNone);
+                    
+            //        // Draw screen border
+            //        if (DataContext.Instance.IsEditMode)
+            //        {
+            //            var rect1 = Global.RenderRect;
+            //            var rect2 = Global.RenderRect;
+            //            rect1.Inflate(1, 1);
+            //            rect2.Inflate(2, 2);
+
+            //            if (Config.Instance.DrawOuterBorder)
+            //            {
+            //                Editor.spriteBatch.Draw(Global.FontDefault.FontImage, rect2, Global.FontDefault.SolidGlyphRectangle, Color.White, 0f, Vector2.Zero, SpriteEffects.None, 0f);
+            //                Editor.spriteBatch.Draw(Global.FontDefault.FontImage, rect1, Global.FontDefault.SolidGlyphRectangle, Color.Black, 0f, Vector2.Zero, SpriteEffects.None, 0f);
+            //            }
+            //        }
+
+            //        Editor.spriteBatch.Draw(Global.RenderOutput, Global.RenderRect, Color.White);
+
+            //        // Draw the editor brush to the screen
+            //        if (DataContext.Instance.IsEditMode)
+            //            if (DataContext.Instance.SelectedTool != null)
+            //                DataContext.Instance.SelectedTool.Brush.EditorDraw(Editor.spriteBatch);
+
+            //        Editor.spriteBatch.End();
+            //    }
+            //}
+        }
+        
+        protected override void Update(GameTime gameTime)
+        {
+            this.gameTime = gameTime;
+            base.Update(gameTime);
+
+            //if (Settings.DoUpdate && !DataContext.Instance.IsEditMode)
+            //{
+            //    Global.GameTimeUpdate = gameTime;
+            //    Global.GameTimeElapsedUpdate = gameTime.ElapsedGameTime.TotalSeconds;
+
+            //    if (!DataContext.Instance.PauseEditMode)
+            //    {
+            //        if (Settings.Input.DoKeyboard)
+            //        {
+            //            Global.KeyboardState.Update(gameTime);
+            //            Global.KeyboardState.Process();
+            //        }
+
+            //        if (Settings.Input.DoMouse)
+            //        {
+            //            Global.MouseState.Update(gameTime);
+            //            Global.MouseState.Process();
+            //        }
+            //    }
+
+            //    Global.CurrentScreen?.Update(gameTime.ElapsedGameTime);
+
+            //    SadConsole.Game.OnUpdate?.Invoke(gameTime);
+            //}
+            //else if (DataContext.Instance.IsEditMode && !DataContext.Instance.PauseEditMode)
+            //{
+            //    DataContext.Instance.Update(gameTime);
+            //}
+        }
+    }
+}
