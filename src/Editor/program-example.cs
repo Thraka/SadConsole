49c9f144a665c1467a4c122c8bcc7a28f12ab579
Andy De George <thraka.accounts@outlook.com>
2018-03-12 14:14:46 -0700

    Basic SadConsole project for an editor

---
+++ src/Editor/program-example.cs
@@ -0,0 +1,59 @@
+using System;
+using SadConsole;
+using Console = SadConsole.Console;
+using Microsoft.Xna.Framework;
+
+namespace MyProject
+{
+    class Program
+    {
+
+        public const int Width = 80;
+        public const int Height = 25;
+
+        static void Main(string[] args)
+        {
+            // Setup the engine and creat the main window.
+            SadConsole.Game.Create("IBM.font", Width, Height);
+
+            // Hook the start event so we can add consoles to the system.
+            SadConsole.Game.OnInitialize = Init;
+
+            // Hook the update event that happens each frame so we can trap keys and respond.
+            SadConsole.Game.OnUpdate = Update;
+                        
+            // Start the game.
+            SadConsole.Game.Instance.Run();
+
+            //
+            // Code here will not run until the game window closes.
+            //
+            
+            SadConsole.Game.Instance.Dispose();
+        }
+        
+        private static void Update(GameTime time)
+        {
+            // Called each logic update.
+
+            // As an example, we'll use the F5 key to make the game full screen
+            if (SadConsole.Global.KeyboardState.IsKeyReleased(Microsoft.Xna.Framework.Input.Keys.F5))
+            {
+                SadConsole.Settings.ToggleFullScreen();
+            }
+        }
+
+        private static void Init()
+        {
+            // Any custom loading and prep. We will use a sample console for now
+
+            Console startingConsole = new Console(Width, Height);
+            startingConsole.FillWithRandomGarbage();
+            startingConsole.Fill(new Rectangle(3, 3, 27, 5), null, Color.Black, 0);
+            startingConsole.Print(6, 5, "Hello from SadConsole", ColorAnsi.CyanBright);
+
+            // Set our new console as the thing to render and process
+            SadConsole.Global.CurrentScreen = startingConsole;
+        }
+    }
+}
