d1885f7da4a205b564cc664c9397e40a67bb6bfb
INeedAUniqueUsername <achen115@ucr.edu>
2018-12-22 16:15:32 -0800

    Add Clear method for line segments

--- src/SadConsole.Shared/Surfaces/SurfaceBase.Editor.cs
+++ src/SadConsole.Shared/Surfaces/SurfaceBase.Editor.cs
@@ -653,25 +653,26 @@ namespace SadConsole.Surfaces
             }
             IsDirty = true;
         }
-
-		/// <summary>
-		/// Draws a single glyph on the console at the specified location.
-		/// </summary>
-		/// <param name="x">X location of the text.</param>
-		/// <param name="y">Y location of the text.</param>
-		/// <param name="glyph">The glyph to display.</param>
-		public void Print(int x, int y, ColoredGlyph glyph) {
-			if (glyph == null)
-				return;
-
-			if (!IsValidCell(x, y, out int index)) return;
-
-			Cell cell = Cells[index];
-			cell.CopyAppearanceFrom(glyph);
-			cell.Glyph = glyph.Glyph;
-			IsDirty = true;
-		}
-
+        
+        /// <summary>
+        /// Draws a single glyph on the console at the specified location.
+        /// </summary>
+        /// <param name="x">X location of the text.</param>
+        /// <param name="y">Y location of the text.</param>
+        /// <param name="glyph">The glyph to display.</param>
+        public void Print(int x, int y, ColoredGlyph glyph)
+        {
+            if (glyph == null)
+                return;
+            
+            if (!IsValidCell(x, y, out int index)) return;
+            
+            Cell cell = Cells[index];
+            cell.CopyAppearanceFrom(glyph);
+            cell.Glyph = glyph.Glyph;
+            IsDirty = true;
+        }
+        
         /// <summary>
         /// Draws the string on the console at the specified location, wrapping if needed.
         /// </summary>
@@ -1138,7 +1139,15 @@ namespace SadConsole.Surfaces
             cell.Background = DefaultBackground;
             IsDirty = true;
         }
-
+        /// <summary>
+        /// Clears a segment of cells, starting from the left and extending to the right. Character is reset to 0, the forground and background is set to default, and effect is set to none. Clears cell decorators.
+        /// </summary>
+        /// <param name="x">The x position of the left end of the segment</param>
+        /// <param name="y">The y position of the segment</param>
+        /// <param name="length">The length of the segment</param>
+        public void Clear(int x, int y, int length) {
+            Clear(new Rectangle(x, y, length, 1));
+        }
         /// <summary>
         /// Clears an area of cells. Character is reset to 0, the forground and background is set to default, and effect is set to none. Clears cell decorators.
         /// </summary>
