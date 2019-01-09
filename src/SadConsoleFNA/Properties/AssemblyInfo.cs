4232b6b28a228ac7b683615b0ea9d4c206ede60d
Eric Tuvesson <eric.tuvesson@gmail.com>
2019-01-09 20:25:00 +0100

    Update projects and add back old FNA project

---
+++ src/SadConsoleFNA/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SadConsole for FNA")]
+[assembly: AssemblyDescription("An FNA library that emulates old-school console and command prompt style graphics.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SadLogic")]
+[assembly: AssemblyProduct("SadConsole")]
+[assembly: AssemblyCopyright("Copyright © 2018 Steve De George JR (Thraka)")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("364c730b-b238-4ed2-a3e3-427a8a5d5f07")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("7.2.0")]
+[assembly: AssemblyFileVersion("7.2.0")]
