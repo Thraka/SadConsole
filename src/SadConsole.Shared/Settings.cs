2f74812080e789284c02201f2c65e50d55eecccc
LtLi0n <31516441+LtLi0n@users.noreply.github.com>
2019-01-10 21:39:15 +0200

    Settings static fields changed to properties.
    The initialization of default values is now handled by a static constructor.

--- src/SadConsole.Shared/Settings.cs
+++ src/SadConsole.Shared/Settings.cs
@@ -10,69 +10,87 @@ namespace SadConsole
     /// </summary>
     public static class Settings
     {
-        internal static bool LoadingEmbeddedFont = false;
+        internal static bool LoadingEmbeddedFont { get; set; }
 
         /// <summary>
         /// The color to automatically clear the device with.
         /// </summary>
-        public static Color ClearColor = Color.Black;
+        public static Color ClearColor { get; set; }
 
         /// <summary>
         /// The type of resizing options for the window.
         /// </summary>
-        public static WindowResizeOptions ResizeMode = WindowResizeOptions.Scale;
+        public static WindowResizeOptions ResizeMode { get; set; }
 
         /// <summary>
         /// Allow the user to resize the window. Must be set before the game is created.
         /// </summary>
-        public static bool AllowWindowResize = true;
+        public static bool AllowWindowResize { get; set; }
 
         /// <summary>
         /// Unlimited FPS when rendering (normally limited to 60fps). Must be set before the game is created.
         /// </summary>
-        public static bool UnlimitedFPS = false;
+        public static bool UnlimitedFPS { get; set; }
 
         /// <summary>
         /// When true, indicates that <see cref="SadConsole.Game.SadConsoleGameComponent.Draw(GameTime)"/> will run.
         /// </summary>
-        public static bool DoDraw = true;
+        public static bool DoDraw { get; set; }
 
         /// <summary>
         /// When true, indicates that <see cref="SadConsole.Game.SadConsoleGameComponent.Draw(GameTime)"/> will render to the screen at the end.
         /// </summary>
-        public static bool DoFinalDraw = true;
+        public static bool DoFinalDraw { get; set; }
 
         /// <summary>
         /// When true, indicates that <see cref="SadConsole.Game.SadConsoleGameComponent.Update(GameTime)"/> will run.
         /// </summary>
-        public static bool DoUpdate = true;
+        public static bool DoUpdate { get; set; }
 
         /// <summary>
         /// The <see cref="Microsoft.Xna.Framework.Graphics.GraphicsProfile"/> value to use.
         /// </summary>
-        public static Microsoft.Xna.Framework.Graphics.GraphicsProfile GraphicsProfile = Microsoft.Xna.Framework.Graphics.GraphicsProfile.Reach;
+        public static Microsoft.Xna.Framework.Graphics.GraphicsProfile GraphicsProfile { get; set; }
 
-        internal static bool IsExitingFullscreen = false;
+        internal static bool IsExitingFullscreen { get; set; }
 
         /// <summary>
         /// Tells MonoGame to use a full screen resolution change instead of soft (quick) full screen. Must be set before the game is created.
         /// </summary>
-        public static bool UseHardwareFullScreen { get; set; } = false;
+        public static bool UseHardwareFullScreen { get; set; }
 
         /// <summary>
         /// When not set to (0,0) this property specifies the minimum size of the game window in pixels.
         /// </summary>
-        public static Point WindowMinimumSize { get; set; } = Point.Zero;
+        public static Point WindowMinimumSize { get; set; }
 
         /// <summary>
         /// When set to true, all loading and saving performed by SadConsole uses GZIP. <see cref="Global.LoadFont(string)"/> does not use this setting and always runs uncompressed.
         /// </summary>
-        public static bool SerializationIsCompressed { get; set; } = false;
+        public static bool SerializationIsCompressed { get; set; }
 
         /// <summary>
         /// When set to true, and a font is not specified with the <see cref="Game.Create(string, int, int, Action{Game})"/> overload, the IBM 8x16 extended SadConsole font will be used.
         /// </summary>
-        public static bool UseDefaultExtendedFont { get; set; } = false;
+        public static bool UseDefaultExtendedFont { get; set; }
+
+        static Settings()
+        {
+            LoadingEmbeddedFont = false;
+            ClearColor = Color.Black;
+            ResizeMode = WindowResizeOptions.Scale;
+            AllowWindowResize = true;
+            UnlimitedFPS = false;
+            DoDraw = true;
+            DoFinalDraw = true;
+            DoUpdate = true;
+            GraphicsProfile = Microsoft.Xna.Framework.Graphics.GraphicsProfile.Reach;
+            IsExitingFullscreen = false;
+            UseHardwareFullScreen = false;
+            WindowMinimumSize = Point.Zero;
+            SerializationIsCompressed = false;
+            UseDefaultExtendedFont = false;
+        }
 
         /// <summary>
         /// Toggles between windowed and fullscreen rendering for SadConsole.
