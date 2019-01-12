40259cb7ab991cc52c02efc2d8d971d8096a3822
Eric Tuvesson <eric.tuvesson@gmail.com>
2019-01-12 16:07:58 +0100

    Add ContentProvider

---
+++ src/SadConsole.Shared/Content/ContentProvider.cs
@@ -0,0 +1,72 @@
+﻿namespace SadConsole.Content
+{
+    using System;
+    using System.IO;
+    using System.Net.Http;
+    using System.Reflection;
+    using System.Threading.Tasks;
+
+    public interface IContentProvider
+    {
+        /// <summary>
+        /// Opens a stream for the given content name.
+        /// </summary>
+        Task<Stream> Open(string name);
+    }
+
+    /// <summary>
+    /// ContentProvider supports reading files, http files and embedded resources.
+    /// </summary>
+    /// <example>
+    /// await contentProvider.Open("https://example.com/logo.png");
+    /// await contentProvider.Open("embedded:SadConsole.Resources.IBM.font");
+    /// await contentProvider.Open("C:\example.png");
+    /// </example>
+    public class ContentProvider : IContentProvider
+    {
+        private readonly Lazy<HttpClient> http;
+
+        /// <summary>
+        /// Initialize a new <see cref="ContentProvider"/>.
+        /// </summary>
+        /// <param name="messageHandler"></param>
+        public ContentProvider(HttpMessageHandler messageHandler = null)
+        {
+            this.http = new Lazy<HttpClient>(() => new HttpClient(messageHandler ?? new HttpClientHandler()));
+        }
+
+        /// <summary>
+        /// Opens a stream for the given content name.
+        /// </summary>
+        public async Task<Stream> Open(string name)
+        {
+            if (name.StartsWith("embedded:", StringComparison.OrdinalIgnoreCase))
+            {
+                var embeddedName = name.Substring(9);
+                return GetAssembly().GetManifestResourceStream(embeddedName);
+            }
+
+            if (name.StartsWith("http://", StringComparison.OrdinalIgnoreCase) ||
+                name.StartsWith("https://", StringComparison.OrdinalIgnoreCase))
+            {
+                var response = await http.Value.GetAsync(name).ConfigureAwait(false);
+                var content = response.EnsureSuccessStatusCode().Content;
+                return await content.ReadAsStreamAsync().ConfigureAwait(false);
+            }
+
+            if (!File.Exists(name))
+                return null;
+
+            return File.OpenRead(name);
+        }
+
+        private static Assembly GetAssembly()
+        {
+#if WINDOWS_UWP || WINDOWS_UAP
+            return typeof(ContentProvider).GetTypeInfo().Assembly;
+#else
+            return Assembly.GetExecutingAssembly();
+#endif
+        }
+    }
+}
