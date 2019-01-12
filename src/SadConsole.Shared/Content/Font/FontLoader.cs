c0a040d3f7c2fb6e35218e0a34e5a8af85e9e48e
Eric Tuvesson <eric.tuvesson@gmail.com>
2019-01-12 16:24:54 +0100

    Add TextureLoader and FontLoader

---
+++ src/SadConsole.Shared/Content/Font/FontLoader.cs
@@ -0,0 +1,39 @@
+﻿namespace SadConsole.Content
+{
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+    using Newtonsoft.Json;
+
+    public class FontLoader
+    {
+        private readonly IContentProvider contentProvider;
+        private readonly TextureLoader textureLoader;
+
+        public FontLoader(IContentProvider contentProvider, TextureLoader textureLoader)
+        {
+            this.contentProvider = contentProvider ?? throw new ArgumentNullException(nameof(contentProvider));
+            this.textureLoader = textureLoader ?? throw new ArgumentNullException(nameof(textureLoader));
+        }
+
+        public async Task<FontMaster> Load(string name)
+        {
+            using (var stream = await contentProvider.Open(name).ConfigureAwait(false))
+            using (var reader = new StreamReader(stream))
+            {
+                var font = (FontMaster)JsonConvert.DeserializeObject(
+                    reader.ReadToEnd(),
+                    typeof(FontMaster),
+                    new JsonSerializerSettings()
+                    {
+                        TypeNameHandling = TypeNameHandling.All
+                    });
+
+                font.Image = await textureLoader.Load(font.FilePath);
+                font.ConfigureRects();
+
+                return font;
+            }
+        }
+    }
+}
---
+++ src/SadConsole.Shared/Content/Texture/TextureLoader.cs
@@ -0,0 +1,31 @@
+﻿namespace SadConsole.Content
+{
+#if XNA
+    using Microsoft.Xna.Framework.Graphics;
+#endif
+
+    using System;
+    using System.Threading.Tasks;
+
+    public class TextureLoader
+    {
+        private readonly IContentProvider contentProvider;
+
+        public TextureLoader(IContentProvider contentProvider)
+        {
+            this.contentProvider = contentProvider ?? throw new ArgumentNullException(nameof(contentProvider));
+        }
+
+        public async Task<Texture2D> Load(string name)
+        {
+            using (var stream = await contentProvider.Open(name).ConfigureAwait(false))
+            {
+#if XNA
+                return Texture2D.FromStream(Global.GraphicsDevice, stream);
+#else
+                throw new NotImplementedException();
+#endif
+            }
+        }
+    }
+}
