389c697cf6e74cc0f961ff882a7b4e85c4d7fd23
SadConsole Master <sadconsole@outlook.com>
2019-02-01 22:59:06 -0800

    Entity manager is more robust with base class methods to trap events. Handles entity.Moved now.

--- src/SadConsole.Shared/Entities/EntityManager.cs
+++ src/SadConsole.Shared/Entities/EntityManager.cs
@@ -18,7 +18,8 @@ namespace SadConsole.Entities
         private bool _hasBeenAdded;
         private Rectangle _cachedView;
         private Dictionary<Entity, EntityState> _entityStates;
-
+        private Console _console;
+        
         /// <summary>
         /// An event to indicate that an entity entered a hotspot.
         /// </summary>
@@ -43,6 +44,11 @@ namespace SadConsole.Entities
         /// An event to indicate that an entity moved within a zone.
         /// </summary>
         public event EventHandler<ZoneMoveEventArgs> MoveZone;
+
+        /// <summary>
+        /// An event to indicate that an entity moved.
+        /// </summary>
+        public event EventHandler<Entity.EntityMovedEventArgs> EntityMoved;
 
         /// <summary>
         /// When <see langword="true"/>, indicates that the attached console is <see cref="ScrollingConsole"/>.
@@ -84,6 +90,7 @@ namespace SadConsole.Entities
             switch (e.Action)
             {
                 case NotifyCollectionChangedAction.Add:
+
                     foreach (var item in e.NewItems)
                     {
                         if (((Console)item).Parent != null) throw new Exception("Object can't be parented to another and added to the entity manager.");
@@ -98,17 +105,39 @@ namespace SadConsole.Entities
                             state.IsInZone = IsPositionZone(state.Position, out Zone zone);
                             state.Zone = zone;
 
+                            ent.Moved += Entity_Moved;
+
                             _entityStates.Add(ent, state);
+                            OnEntityAdded(_console, ent);
                         }
                     }
                     break;
+
                 case NotifyCollectionChangedAction.Remove:
 
                     foreach (var item in e.OldItems)
+                    {
                         if (item is Entity ent)
+                        {
                             _entityStates.Remove(ent);
+                            ent.Moved -= Entity_Moved;
+                            OnEntityRemoved(_console, ent);
+                        }
+                    }
                     break;
+
                 case NotifyCollectionChangedAction.Replace:
+
+                    foreach (var item in e.OldItems)
+                    {
+                        if (item is Entity ent)
+                        {
+                            _entityStates.Remove(ent);
+                            ent.Moved -= Entity_Moved;
+                            OnEntityRemoved(_console, ent);
+                        }
+                    }
+
                     foreach (var item in e.NewItems)
                     { 
                         if (((Console)item).Parent != null) throw new Exception("Object can't be parented to another and added to the entity manager.");
@@ -123,14 +152,14 @@ namespace SadConsole.Entities
                             state.IsInZone = IsPositionZone(state.Position, out Zone zone);
                             state.Zone = zone;
 
+                            ent.Moved += Entity_Moved;
+
                             _entityStates.Add(ent, state);
+                            OnEntityAdded(_console, ent);
                         }
                     }
-
-                    foreach (var item in e.OldItems)
-                        if (item is Entity ent)
-                            _entityStates.Remove(ent);
                     break;
+
                 case NotifyCollectionChangedAction.Move:
                     break;
                 case NotifyCollectionChangedAction.Reset:
@@ -139,6 +168,9 @@ namespace SadConsole.Entities
                     throw new ArgumentOutOfRangeException();
             }
         }
+
+        private void Entity_Moved(object sender, Entity.EntityMovedEventArgs e) =>
+            EvaluateEntityState((Entity)sender);
 
         private bool IsPositionZone(in Point point, out Zone zone)
         {
@@ -216,80 +248,95 @@ namespace SadConsole.Entities
                 }
 
                 entity.Update(delta);
+            }
+        }
 
-                var state = _entityStates[entity];
-                
-                // If this entity is disabled, it's not being watched for zone/hotspot interaction.
-                if (state.IsDisabled) continue;
+        private void EvaluateEntityState(Entity entity)
+        {
+            var state = _entityStates[entity];
 
-                // Check if entity has moved
-                if (entity.Position != state.Position)
+            // If this entity is disabled, it's not being watched for zone/hotspot interaction.
+            if (state.IsDisabled) return;
+
+            // Check if entity has moved
+            if (entity.Position != state.Position)
+            {
+                var isHotspot = IsPositionHotspot(entity.Position, out var spot);
+                var isZone = IsPositionZone(entity.Position, out var zone);
+
+                OnEntityMoved(entity, entity.Position, state.Position);
+                EntityMoved?.Invoke(this, new Entity.EntityMovedEventArgs(entity, state.Position));
+
+                // Still in zone
+                if (state.IsInZone && isZone)
                 {
-                    var isHotspot = IsPositionHotspot(entity.Position, out Hotspot spot);
-                    var isZone = IsPositionZone(entity.Position, out Zone zone);
-
-                    // Still in zone
-                    if (state.IsInZone && isZone)
+                    // Same zone -- trigger move event
+                    if (state.Zone == zone)
                     {
-                        // Same zone -- trigger move event
-                        if (state.Zone == zone)
-                        {
-                            MoveZone?.Invoke(this, new ZoneMoveEventArgs(console, zone, entity, entity.Position, state.Position));
-                        }
-
-                        // New zone -- trigger exit and enter event.
-                        else
-                        {
-                            ExitZone?.Invoke(this, new ZoneEventArgs(console, state.Zone, entity, state.Position));
-                            EnterZone?.Invoke(this, new ZoneEventArgs(console, zone, entity, entity.Position));
-
-                            state.Zone = zone;
-                        }
+                        OnEntityMoveZone(_console, zone, entity, entity.Position, state.Position);
+                        MoveZone?.Invoke(this, new ZoneMoveEventArgs(_console, zone, entity, entity.Position, state.Position));
                     }
 
-                    // Left zone
-                    else if (state.IsInZone)
+                    // New zone -- trigger exit and enter event.
+                    else
                     {
-                        ExitZone?.Invoke(this, new ZoneEventArgs(console, state.Zone, entity, state.Position));
-                        state.IsInZone = false;
-                        state.Zone = null;
-                    }
+                        OnEntityExitZone(_console, state.Zone, entity, entity.Position);
+                        OnEntityEnterZone(_console, zone, entity, entity.Position);
+                        ExitZone?.Invoke(this, new ZoneEventArgs(_console, state.Zone, entity, state.Position));
+                        EnterZone?.Invoke(this, new ZoneEventArgs(_console, zone, entity, entity.Position));
 
-                    // Entered zone
-                    else if (isZone)
-                    {
-                        EnterZone?.Invoke(this, new ZoneEventArgs(console, zone, entity, entity.Position));
-                        state.IsInZone = true;
                         state.Zone = zone;
                     }
-
-                    // Still in hotspot
-                    if (state.IsInHotspot && isHotspot)
-                    {
-                        ExitHotspot?.Invoke(this, new HotspotEventArgs(console, state.Hotspot, entity, state.Position));
-                        EnterHotspot?.Invoke(this, new HotspotEventArgs(console, spot, entity, entity.Position));
-
-                        state.Hotspot = spot;
-                    }
-
-                    // Left hotspot
-                    else if (state.IsInHotspot)
-                    {
-                        ExitHotspot?.Invoke(this, new HotspotEventArgs(console, state.Hotspot, entity, state.Position));
-
-                        state.IsInHotspot = false;
-                        state.Hotspot = null;
-                    }
-                    else if (isHotspot)
-                    {
-                        EnterHotspot?.Invoke(this, new HotspotEventArgs(console, spot, entity, entity.Position));
-
-                        state.IsInHotspot = true;
-                        state.Hotspot = spot;
-                    }
-
-                    state.Position = entity.Position;
                 }
+
+                // Left zone
+                else if (state.IsInZone)
+                {
+                    OnEntityExitZone(_console, state.Zone, entity, entity.Position);
+                    ExitZone?.Invoke(this, new ZoneEventArgs(_console, state.Zone, entity, state.Position));
+                    state.IsInZone = false;
+                    state.Zone = null;
+                }
+
+                // Entered zone
+                else if (isZone)
+                {
+                    OnEntityEnterZone(_console, zone, entity, entity.Position);
+                    EnterZone?.Invoke(this, new ZoneEventArgs(_console, zone, entity, entity.Position));
+                    state.IsInZone = true;
+                    state.Zone = zone;
+                }
+
+                // Still in hotspot
+                if (state.IsInHotspot && isHotspot)
+                {
+                    OnEntityExitHotspot(_console, state.Hotspot, entity, state.Position);
+                    OnEntityEnterHotspot(_console, spot, entity, entity.Position);
+                    ExitHotspot?.Invoke(this, new HotspotEventArgs(_console, state.Hotspot, entity, state.Position));
+                    EnterHotspot?.Invoke(this, new HotspotEventArgs(_console, spot, entity, entity.Position));
+
+                    state.Hotspot = spot;
+                }
+
+                // Left hotspot
+                else if (state.IsInHotspot)
+                {
+                    OnEntityExitHotspot(_console, state.Hotspot, entity, state.Position);
+                    ExitHotspot?.Invoke(this, new HotspotEventArgs(_console, state.Hotspot, entity, state.Position));
+
+                    state.IsInHotspot = false;
+                    state.Hotspot = null;
+                }
+                else if (isHotspot)
+                {
+                    OnEntityEnterHotspot(_console, spot, entity, entity.Position);
+                    EnterHotspot?.Invoke(this, new HotspotEventArgs(_console, spot, entity, entity.Position));
+
+                    state.IsInHotspot = true;
+                    state.Hotspot = spot;
+                }
+
+                state.Position = entity.Position;
             }
         }
 
@@ -313,9 +360,7 @@ namespace SadConsole.Entities
         {
             foreach (var entity in Entities)
             {
-                var transformedState = new MouseConsoleState(entity, state.Mouse);
-
-                if (entity.ProcessMouse(transformedState))
+                if (entity.ProcessMouse(new MouseConsoleState(entity, state.Mouse)))
                 {
                     handled = true;
                     return;
@@ -336,6 +381,8 @@ namespace SadConsole.Entities
                 HasViewport = true;
                 _cachedView = view.ViewPort;
             }
+
+            _console = console;
         }
 
         /// <inheritdoc />
@@ -343,6 +390,7 @@ namespace SadConsole.Entities
         {
             _hasBeenAdded = false;
             _cachedView = default;
+            _console = null;
         }
 
         /// <summary>
@@ -378,6 +426,74 @@ namespace SadConsole.Entities
 
             return _entityStates[entity].IsDisabled;
         }
+
+        /// <summary>
+        /// Called when an entity is added to the manager.
+        /// </summary>
+        /// <param name="host">The console this manager is attached to.</param>
+        /// <param name="entity">The entity added.</param>
+        protected virtual void OnEntityAdded(Console host, Entity entity) { }
+
+        /// <summary>
+        /// Called when an entity is removed from the manager.
+        /// </summary>
+        /// <param name="host">The console this manager is attached to.</param>
+        /// <param name="entity">The entity removed.</param>
+        protected virtual void OnEntityRemoved(Console host, Entity entity) { }
+
+        /// <summary>
+        /// Called when an entity enters a hotspot.
+        /// </summary>
+        /// <param name="host">The host console that the hotspot and entity share.</param>
+        /// <param name="hotspot">The hotspot the entity entered.</param>
+        /// <param name="entity">The entity that entered a hotspot.</param>
+        /// <param name="triggeredPosition">The position within the <see cref="Hotspot.Positions"/>.</param>
+        protected virtual void OnEntityEnterHotspot(Console host, Hotspot hotspot, Entity entity, Point triggeredPosition) { }
+
+        /// <summary>
+        /// Called when an entity exits a hotspot.
+        /// </summary>
+        /// <param name="host">The host console that the hotspot and entity share.</param>
+        /// <param name="hotspot">The hotspot the entity exited.</param>
+        /// <param name="entity">The entity that exited a hotspot.</param>
+        /// <param name="triggeredPosition">The position within the <see cref="Hotspot.Positions"/>.</param>
+        protected virtual void OnEntityExitHotspot(Console host, Hotspot hotspot, Entity entity, Point triggeredPosition) { }
+
+        /// <summary>
+        /// Called when an entity enters a zone.
+        /// </summary>
+        /// <param name="host">The host console that the zone and entity share.</param>
+        /// <param name="zone">The zone the entity entered.</param>
+        /// <param name="entity">The entity that entered the zone.</param>
+        /// <param name="triggeredPosition">The position the entity entered.</param>
+        protected virtual void OnEntityEnterZone(Console host, Zone zone, Entity entity, Point triggeredPosition) { }
+
+        /// <summary>
+        /// Called when an entity enters a zone.
+        /// </summary>
+        /// <param name="host">The host console that the zone and entity share.</param>
+        /// <param name="zone">The zone the entity exited.</param>
+        /// <param name="entity">The entity that exited the zone.</param>
+        /// <param name="triggeredPosition">The new position the entity left.</param>
+        protected virtual void OnEntityExitZone(Console host, Zone zone, Entity entity, Point triggeredPosition) { }
+
+        /// <summary>
+        /// Called when an entity moves within a zone.
+        /// </summary>
+        /// <param name="host">The host console that the zone and entity share.</param>
+        /// <param name="zone">The zone the entity moved in.</param>
+        /// <param name="entity">The entity that moved in the zone.</param>
+        /// <param name="newPosition">The position the entity moved to.</param>
+        /// <param name="oldPosition">The position the entity moved from.</param>
+        protected virtual void OnEntityMoveZone(Console host, Zone zone, Entity entity, Point newPosition, Point oldPosition) { }
+
+        /// <summary>
+        /// Creates a new event args for the entity movement.
+        /// </summary>
+        /// <param name="entity">The entity associated with the event.</param>
+        /// <param name="newPosition">The position the entity moved to.</param>
+        /// <param name="oldPosition">The position the entity moved from.</param>
+        protected virtual void OnEntityMoved(Entity entity, Point newPosition, Point oldPosition) { }
 
         private class EntityState
         {
