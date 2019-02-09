d4dd95acb45272706c289fac7304e79a5cf99580
SadConsole Master <sadconsole@outlook.com>
2019-02-09 11:28:20 -0800

    Add new component that does entity positionoffset sync

---
+++ src/SadConsole.Shared/Entities/EntityViewSyncComponent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace SadConsole.Entities
+{
+    /// <summary>
+    /// Add to an <see cref="Entity"/> to sync the visibility and position offset with a parent <see cref="Console"/>. 
+    /// </summary>
+    public class EntityViewSyncComponent : UpdateConsoleComponent
+    {
+        /// <inheritdoc />
+        public override void Update(Console console, TimeSpan delta)
+        {
+            if (console.Parent is IConsoleViewPort parent)
+            {
+                var parentViewPort = parent.ViewPort;
+                ((Entity)console).PositionOffset = new Point(-parentViewPort.Location.X, -parentViewPort.Location.Y);
+                console.IsVisible = parentViewPort.Contains(console.Position);
+            }
+            else
+            {
+                console.IsVisible = console.Position.X >= 0 && console.Position.Y >= 0 &&
+                                    console.Position.X < console.Parent.Width && console.Position.Y < console.Parent.Height;
+            }
+        }
+
+        /// <inheritdoc />
+        public override void OnAdded(Console console)
+        {
+            if (!(console is Entity))
+                throw new Exception($"{nameof(EntityViewSyncComponent)} can only be added to an entity.");
+        }
+    }
+}
