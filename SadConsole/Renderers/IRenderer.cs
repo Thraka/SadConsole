a0f50fbaa248f60a0f1828f879e233b98de78002
Andy De George <thraka.junk@outlook.com>
2019-11-11 13:15:34 -0800

    Descriptions for renderer

--- SadConsole/Renderers/IRenderer.cs
+++ SadConsole/Renderers/IRenderer.cs
@@ -5,14 +5,33 @@ using SadRogue.Primitives;
 
 namespace SadConsole.Renderers
 {
+    /// <summary>
+    /// Draws a surface.
+    /// </summary>
     public interface IRenderer: IDisposable
     {
+        /// <summary>
+        /// Called when the renderer is added to a surface.
+        /// </summary>
+        /// <param name="surfaceObject">The surface.</param>
         void Attach(ScreenObjectSurface surfaceObject);
 
+        /// <summary>
+        /// Called when the renderer is removed from a surface.
+        /// </summary>
+        /// <param name="surfaceObject">The surface.</param>
         void Detatch(ScreenObjectSurface surfaceObject);
 
+        /// <summary>
+        /// Refreshes a cached drawing state.
+        /// </summary>
+        /// <param name="surfaceObject">The surface this renderer is attached to.</param>
         void Refresh(ScreenObjectSurface surfaceObject);
 
+        /// <summary>
+        /// Creates a drawcall in the drawing pipeline.
+        /// </summary>
+        /// <param name="surfaceObject">The surface this renderer is attached to.</param>
         void Render(ScreenObjectSurface surfaceObject);
     }
 }
