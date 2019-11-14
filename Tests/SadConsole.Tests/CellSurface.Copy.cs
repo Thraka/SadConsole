e9a2f36b7b07e8c79ae9b87c12dfd1ad97a3bc68
Andy De George <thraka.junk@outlook.com>
2019-11-13 16:48:37 -0800

    Add test project

---
+++ Tests/SadConsole.Tests/CellSurface.Copy.cs
@@ -0,0 +1,46 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SadConsole.Tests
+{
+    public partial class CellSurface
+    {
+        [TestMethod]
+        public void Copy_SameSizeRegion_Exact()
+        {
+            var surface1 = new SadConsole.CellSurface(20, 20);
+            var surface2 = new SadConsole.CellSurface(20, 20);
+
+            surface1.FillWithRandomGarbage();
+            surface1.Copy(surface2);
+
+            for (int i = 0; i < surface1.Cells.Length; i++)
+            {
+                Assert.IsTrue(surface1[i].Equals(surface2[i]));
+            }
+        }
+
+        [TestMethod]
+        public void Copy_SameSizeRegion_IntoBigger()
+        {
+            var surface1 = new SadConsole.CellSurface(20, 20);
+            var surface2 = new SadConsole.CellSurface(22, 22);
+
+            ColoredGlyph defaultCell = new ColoredGlyph();
+            surface2[0].CopyAppearanceTo(defaultCell);
+
+            surface1.FillWithRandomGarbage();
+            surface1.Copy(surface2);
+
+            for (int y = 0; y < surface2.BufferHeight; y++)
+            {
+                for (int x = 0; x < surface2.BufferWidth; x++)
+                {
+                    if (x > surface1.BufferWidth - 1 || y > surface1.BufferHeight - 1)
+                        Assert.IsTrue(surface2[x, y].Equals(defaultCell));
+                    else
+                        Assert.IsTrue(surface1[x, y].Equals(surface2[x, y]));
+                }
+            }
+        }
+    }
+}
---
+++ Tests/SadConsole.Tests/CellSurface.Create.cs
@@ -0,0 +1,76 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SadConsole.Tests
+{
+    [TestClass]
+    public partial class CellSurface
+    {
+        [DataTestMethod]
+        [DataRow(1, 1)]
+        [DataRow(10, 10)]
+        [DataRow(100, 100)]
+        public void Create_WithSize(int width, int height)
+        {
+            var surface = new SadConsole.CellSurface(width, height);
+            Assert.IsTrue(surface.BufferWidth == width, $"BufferWidth does not match width");
+            Assert.IsTrue(surface.BufferWidth == height, $"BufferHeight does not match height");
+            Assert.IsTrue(surface.ViewPosition == new SadRogue.Primitives.Point(0, 0));
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 1, 10, 10)]
+        [DataRow(10, 10, 100, 100)]
+        [DataRow(100, 100, 1000, 1000)]
+        public void Create_WithViewSize(int width, int height, int bufferWidth, int bufferHeight)
+        {
+            var surface = new SadConsole.CellSurface(width, height, bufferWidth, bufferHeight);
+            Assert.IsTrue(surface.BufferWidth == bufferWidth, $"BufferWidth does not match value");
+            Assert.IsTrue(surface.BufferWidth == bufferHeight, $"BufferHeight does not match value");
+            Assert.IsTrue(surface.ViewWidth == width, $"ViewWidth does not match value");
+            Assert.IsTrue(surface.ViewHeight == height, $"ViewHeight does not match value");
+            Assert.IsTrue(surface.ViewPosition == new SadRogue.Primitives.Point(0, 0));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentOutOfRangeException))]
+        public void Create_WidthZero_IsException()
+        {
+            new SadConsole.CellSurface(0, 10);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentOutOfRangeException))]
+        public void Create_HeightZero_IsException()
+        {
+            new SadConsole.CellSurface(10, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentOutOfRangeException))]
+        public void Create_BufferHeightZero_IsException()
+        {
+            new SadConsole.CellSurface(1, 1, 10, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentOutOfRangeException))]
+        public void Create_BufferWidthZero_IsException()
+        {
+            new SadConsole.CellSurface(1, 1, 0, 10);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentOutOfRangeException))]
+        public void Create_ViewBiggerThanBufferWidth_IsException()
+        {
+            new SadConsole.CellSurface(11, 1, 10, 10);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentOutOfRangeException))]
+        public void Create_ViewBiggerThanBufferHeight_IsException()
+        {
+            new SadConsole.CellSurface(1, 11, 10, 10);
+        }
+    }
+}
