65c45b92fdcd233d298e16c958a3ba727b58661b
SadConsole Master <sadconsole@outlook.com>
2020-04-21 14:51:58 -0700

    Create dotnet templates

---
+++ templates/template_code/SadConsole8.Console.CSharp/MyConsole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SadConsole;
+using Microsoft.Xna.Framework;
+using Console = SadConsole.Console;
+
+namespace MyGame
+{
+    class MyConsole: Console
+    {
+
+        public MyConsole(): base(80, 25)
+        {
+
+        }
+    }
+}
---
+++ templates/template_code/SadConsole8.Project.CSharp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using SadConsole;
+using Microsoft.Xna.Framework;
+using Console = SadConsole.Console;
+
+namespace MyGame
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Setup the engine and create the main window.
+            SadConsole.Game.Create(80, 25);
+
+            // Hook the start event so we can add consoles to the system.
+            SadConsole.Game.OnInitialize = Init;
+
+            // Start the game.
+            SadConsole.Game.Instance.Run();
+            SadConsole.Game.Instance.Dispose();
+        }
+
+        static void Init()
+        {
+            var console = new Console(80, 25);
+            console.FillWithRandomGarbage();
+            console.Fill(new Rectangle(3, 3, 23, 3), Color.Violet, Color.Black, 0, 0);
+            console.Print(4, 4, "Hello from SadConsole");
+
+            SadConsole.Global.CurrentScreen = console;
+        }
+    }
+}
