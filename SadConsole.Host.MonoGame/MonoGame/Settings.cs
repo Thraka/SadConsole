36681f7cb46a0651dbd8973ed52040a46c916d6d
Andy De George <thraka.junk@outlook.com>
2020-07-24 16:42:56 -0700

    Changed monogame blendstate to be the same as SFML

--- SadConsole.Host.MonoGame/MonoGame/Settings.cs
+++ SadConsole.Host.MonoGame/MonoGame/Settings.cs
@@ -15,11 +15,28 @@ namespace SadConsole.Host
         /// <summary>
         /// The blend state used with <see cref="SadConsole.Renderers.IRenderer"/> on surfaces.
         /// </summary>
-        public static Microsoft.Xna.Framework.Graphics.BlendState MonoGameSurfaceBlendState { get; set; } = Microsoft.Xna.Framework.Graphics.BlendState.Additive;
+        public static Microsoft.Xna.Framework.Graphics.BlendState MonoGameSurfaceBlendState { get; set; }
+            = new Microsoft.Xna.Framework.Graphics.BlendState()
+            {
+                AlphaBlendFunction = Microsoft.Xna.Framework.Graphics.BlendFunction.Add,
+                AlphaDestinationBlend = Microsoft.Xna.Framework.Graphics.Blend.InverseSourceAlpha,
+                AlphaSourceBlend = Microsoft.Xna.Framework.Graphics.Blend.One,
+                ColorBlendFunction = Microsoft.Xna.Framework.Graphics.BlendFunction.Add,
+                ColorDestinationBlend = Microsoft.Xna.Framework.Graphics.Blend.InverseSourceAlpha,
+                ColorSourceBlend = Microsoft.Xna.Framework.Graphics.Blend.SourceAlpha
+            };
 
         /// <summary>
         /// The blend state used when drawing surfaces to the screen.
         /// </summary>
-        public static Microsoft.Xna.Framework.Graphics.BlendState MonoGameScreenBlendState { get; set; } = Microsoft.Xna.Framework.Graphics.BlendState.NonPremultiplied;
+        public static Microsoft.Xna.Framework.Graphics.BlendState MonoGameScreenBlendState { get; set; } = new Microsoft.Xna.Framework.Graphics.BlendState()
+        {
+            AlphaBlendFunction = Microsoft.Xna.Framework.Graphics.BlendFunction.Add,
+            AlphaDestinationBlend = Microsoft.Xna.Framework.Graphics.Blend.InverseSourceAlpha,
+            AlphaSourceBlend = Microsoft.Xna.Framework.Graphics.Blend.One,
+            ColorBlendFunction = Microsoft.Xna.Framework.Graphics.BlendFunction.Add,
+            ColorDestinationBlend = Microsoft.Xna.Framework.Graphics.Blend.InverseSourceAlpha,
+            ColorSourceBlend = Microsoft.Xna.Framework.Graphics.Blend.SourceAlpha
+        };
     }
 }
