d0b78045f3b224647e86343f49fb427763deab46
Andy De George <thraka.junk@outlook.com>
2020-09-26 11:58:07 -0700

    Add docs for adjustable color and make it a bit more robust

--- SadConsole/UI/AdjustableColor.cs
+++ SadConsole/UI/AdjustableColor.cs
@@ -3,21 +3,28 @@ using static SadConsole.UI.Colors;
 
 namespace SadConsole.UI
 {
-    //TODO: Document
+    /// <summary>
+    /// A color that can be adjusted by brightness and mapped to a <see cref="Colors"/> color.
+    /// </summary>
     public class AdjustableColor
     {
         /// <summary>
-        /// The name of the theme color.
+        /// The name of the color.
         /// </summary>
-        public string Title { get; set; }
-        public Color BaseColor { get; set; }
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The color without brightness.
+        /// </summary>
+        public Color BaseColor { get; private set; }
+
+        /// <summary>
+        /// The color with brightness applied.
+        /// </summary>
         public Color ComputedColor
         {
             get
             {
-                if (IsCustomColor)
-                    return BaseColor;
-
                 return Brightness switch
                 {
                     Brightness.Brightest => BaseColor.GetBrightest(),
@@ -28,16 +35,83 @@ namespace SadConsole.UI
                 };
             }
         }
+
+        /// <summary>
+        /// <see langword="true"/> when this color is not defined by a <see cref="Colors"/> object; otherwise <see langword="false"/>.
+        /// </summary>
         public bool IsCustomColor { get; set; }
+
+        /// <summary>
+        /// The <see cref="Colors"/> mapped color when <see cref="IsCustomColor"/> is <see langword="false"/>.
+        /// </summary>
         public ColorNames UIColor { get; private set; }
 
-        public Brightness Brightness { get; set; } = Brightness.Normal;
+        /// <summary>
+        /// A brightness to apply to the color.
+        /// </summary>
+        public Brightness Brightness { get; set; }
 
-        public AdjustableColor(Color color, string title, Colors colors)
+        /// <summary>
+        /// Creates a color that isn't mapped to a <see cref="Colors"/> color.
+        /// </summary>
+        /// <param name="color">The color.</param>
+        /// <param name="name">A friendly name of the color.</param>
+        public AdjustableColor(Color color, string name)
+        {
+            Name = name;
+            SetColor(color);
+        }
+
+        /// <summary>
+        /// Creates a color and tries to map it to a <see cref="Colors"/> color.
+        /// </summary>
+        /// <param name="color">The color.</param>
+        /// <param name="name">A friendly name of the color.</param>
+        /// <param name="colors">The colors to try and map to.</param>
+        public AdjustableColor(Color color, string name, Colors colors)
+        {
+            Name = name;
+            SetColor(color, colors);
+        }
+
+        /// <summary>
+        /// Creates a color that maps to a <see cref="Colors"/> color.
+        /// </summary>
+        /// <param name="color">The predefined color.</param>
+        /// <param name="name">A friendly name of the color.</param>
+        /// <param name="colors">The colors used setting the <see cref="BaseColor"/> value.</param>
+        public AdjustableColor(ColorNames color, string name, Colors colors)
+        {
+            UIColor = color;
+            SetUIColor(color, colors);
+            Name = name;
+        }
+
+        private AdjustableColor() { }
+
+        /// <summary>
+        /// Maps this adjustable color to a <see cref="Colors"/> color.
+        /// </summary>
+        /// <param name="color">The predefined color.</param>
+        /// <param name="colors">The colors used setting the <see cref="BaseColor"/> value.</param>
+        public void SetUIColor(ColorNames color, Colors colors)
+        {
+            UIColor = color;
+            IsCustomColor = false;
+            BaseColor = colors.FromColorName(color);
+            Brightness = Brightness.Normal;
+        }
+
+        /// <summary>
+        /// Tries to map this adjustable color to a <see cref="Colors"/> color.
+        /// </summary>
+        /// <param name="color">The color.</param>
+        /// <param name="colors">The colors to try and map to.</param>
+        public void SetColor(Color color, Colors colors)
         {
             BaseColor = color;
-            Title = title;
             IsCustomColor = true;
+            Brightness = Brightness.Normal;
 
             if (colors.TryToColorName(color, out ColorNames colorName))
             {
@@ -46,18 +120,38 @@ namespace SadConsole.UI
             }
         }
 
-        public AdjustableColor(ColorNames color, string title, Colors colors)
+        /// <summary>
+        /// Configures this adjustable color to the specified color.
+        /// </summary>
+        /// <param name="color">The color.</param>
+        public void SetColor(Color color)
         {
-            UIColor = color;
-            SetUIColor(color, colors);
-            Title = title;
+            BaseColor = color;
+            IsCustomColor = true;
+            Brightness = Brightness.Normal;
         }
 
-        public void SetUIColor(ColorNames color, Colors colors)
+        /// <summary>
+        /// Creates a copy of this object.
+        /// </summary>
+        /// <returns>A new adjustable color object.</returns>
+        public AdjustableColor Clone()
         {
-            UIColor = color;
-            IsCustomColor = false;
-            BaseColor = colors.FromColorName(color);
+            return new AdjustableColor()
+            {
+                Name = Name,
+                BaseColor = BaseColor,
+                IsCustomColor = IsCustomColor,
+                UIColor = UIColor,
+                Brightness = Brightness
+            };
         }
+
+        /// <summary>
+        /// Casts this object to a <see cref="Color"/>.
+        /// </summary>
+        /// <param name="color">The adjustable color.</param>
+        public static implicit operator Color(AdjustableColor color) =>
+            color.ComputedColor;
     }
 }
