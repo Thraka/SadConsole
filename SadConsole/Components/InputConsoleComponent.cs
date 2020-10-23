18bc1924a26c785352e8a6257d1770457dba65ec
Andy De George <thraka.junk@outlook.com>
2020-10-22 22:06:18 -0700

    Split main components out to individual files.

---
+++ SadConsole/Components/InputConsoleComponent.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using SadConsole.Input;
+
+namespace SadConsole.Components
+{
+    /// <summary>
+    /// A base class that implements <see cref="IComponent.ProcessMouse(IScreenObject, MouseScreenObjectState, out bool)"/> and <see cref="IComponent.ProcessKeyboard(IScreenObject, Keyboard, out bool)"/> of <see cref="IComponent"/>.
+    /// </summary>
+    public abstract class InputConsoleComponent : IComponent
+    {
+        /// <summary>
+        /// Indicates priority to other components.
+        /// </summary>
+        public int SortOrder { get; set; }
+
+        /// <summary>
+        /// Called by a host when the keyboard is being processed.
+        /// </summary>
+        /// <param name="host">The host calling the component.</param>
+        /// <param name="keyboard">The state of the keyboard.</param>
+        /// <param name="handled">When set to <see langword="true"/> informs the host caller that we handled the mouse and to stop others from handling.</param>
+        public abstract void ProcessKeyboard(IScreenObject host, Keyboard keyboard, out bool handled);
+
+        /// <summary>
+        /// Called by a host when the mouse is being processed.
+        /// </summary>
+        /// <param name="host">The host calling the component.</param>
+        /// <param name="state">The state of the mouse in relation to the console.</param>
+        /// <param name="handled">When set to <see langword="true"/> informs the host caller that we handled the mouse and to stop others from handling.</param>
+        public abstract void ProcessMouse(IScreenObject host, MouseScreenObjectState state, out bool handled);
+
+        /// <inheritdoc />
+        public virtual void OnAdded(IScreenObject host) { }
+
+        /// <inheritdoc />
+        public virtual void OnRemoved(IScreenObject host) { }
+
+        int IComponent.SortOrder => SortOrder;
+
+        bool IComponent.IsUpdate => false;
+
+        bool IComponent.IsRender => false;
+
+        bool IComponent.IsMouse => true;
+
+        bool IComponent.IsKeyboard => true;
+
+        void IComponent.Render(IScreenObject host, TimeSpan delta) { }
+
+        void IComponent.ProcessKeyboard(IScreenObject host, Keyboard keyboard, out bool handled) =>
+            ProcessKeyboard(host, keyboard, out handled);
+
+        void IComponent.ProcessMouse(IScreenObject host, MouseScreenObjectState mouse, out bool handled) =>
+            ProcessMouse(host, mouse, out handled);
+
+        void IComponent.Update(IScreenObject host, TimeSpan delta) { }
+    }
+}
---
+++ SadConsole/Components/LogicComponent.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using SadConsole.Input;
+
+namespace SadConsole.Components
+{
+    /// <summary>
+    /// A base class that implements <see cref="IComponent.Update(IScreenObject, TimeSpan)"/> and <see cref="IComponent.Render(IScreenObject, TimeSpan)"/> of <see cref="IComponent"/>.
+    /// </summary>
+    public abstract class LogicComponent : IComponent
+    {
+        /// <summary>
+        /// Indicates priority to other components.
+        /// </summary>
+        public int SortOrder { get; set; }
+
+        /// <summary>
+        /// Called by a host on the draw frame.
+        /// </summary>
+        /// <param name="host">The host calling the component.</param>
+        /// <param name="delta">The time that has elapsed from the last call to this component.</param>
+        public abstract void Render(IScreenObject host, TimeSpan delta);
+
+        /// <summary>
+        /// Called by a host on the update frame.
+        /// </summary>
+        /// <param name="host">The host calling the component.</param>
+        /// <param name="delta">The time that has elapsed from the last call to this component.</param>
+        public abstract void Update(IScreenObject host, TimeSpan delta);
+
+        /// <inheritdoc />
+        public virtual void OnAdded(IScreenObject host) { }
+
+        /// <inheritdoc />
+        public virtual void OnRemoved(IScreenObject host) { }
+
+        int IComponent.SortOrder => SortOrder;
+
+        bool IComponent.IsUpdate => true;
+
+        bool IComponent.IsRender => true;
+
+        bool IComponent.IsMouse => false;
+
+        bool IComponent.IsKeyboard => false;
+
+        void IComponent.Render(IScreenObject host, TimeSpan delta) =>
+            Render(host, delta);
+
+        void IComponent.ProcessKeyboard(IScreenObject host, Keyboard keyboard, out bool handled) =>
+            handled = false;
+
+        void IComponent.ProcessMouse(IScreenObject host, MouseScreenObjectState state, out bool handled) =>
+            handled = false;
+
+        void IComponent.Update(IScreenObject host, TimeSpan delta) =>
+            Update(host, delta);
+    }
+}
