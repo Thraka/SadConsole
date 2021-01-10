5e3cd8213f975b19e6a84749bc93e102bf6c915e
Andy De George <thraka.junk@outlook.com>
2021-01-10 12:34:49 -0800

    Fix bug with clearing components from object

--- SadConsole/ScreenObject.cs
+++ SadConsole/ScreenObject.cs
@@ -388,42 +388,47 @@ namespace SadConsole
                     while (ComponentsRender.Count != 0)
                     {
                         ComponentsRender[0].OnRemoved(this);
-                        FilterRemoveItem(ComponentsRender[0]);
 
                         if (!items.Contains(ComponentsRender[0]))
                             items.Add(ComponentsRender[0]);
+
+                        FilterRemoveItem(ComponentsRender[0]);
                     }
                     while (ComponentsUpdate.Count != 0)
                     {
                         ComponentsUpdate[0].OnRemoved(this);
-                        FilterRemoveItem(ComponentsUpdate[0]);
 
                         if (!items.Contains(ComponentsUpdate[0]))
                             items.Add(ComponentsUpdate[0]);
+
+                        FilterRemoveItem(ComponentsUpdate[0]);
                     }
                     while (ComponentsKeyboard.Count != 0)
                     {
                         ComponentsKeyboard[0].OnRemoved(this);
-                        FilterRemoveItem(ComponentsKeyboard[0]);
 
                         if (!items.Contains(ComponentsKeyboard[0]))
                             items.Add(ComponentsKeyboard[0]);
+
+                        FilterRemoveItem(ComponentsKeyboard[0]);
                     }
                     while (ComponentsMouse.Count != 0)
                     {
                         ComponentsMouse[0].OnRemoved(this);
-                        FilterRemoveItem(ComponentsMouse[0]);
 
                         if (!items.Contains(ComponentsMouse[0]))
                             items.Add(ComponentsMouse[0]);
+
+                        FilterRemoveItem(ComponentsMouse[0]);
                     }
                     while (ComponentsEmpty.Count != 0)
                     {
                         ComponentsEmpty[0].OnRemoved(this);
-                        FilterRemoveItem(ComponentsEmpty[0]);
 
                         if (!items.Contains(ComponentsEmpty[0]))
                             items.Add(ComponentsEmpty[0]);
+
+                        FilterRemoveItem(ComponentsEmpty[0]);
                     }
 
                     foreach (IComponent item in items)
