73161fe51a945aede57ef083e550a09ddc9453c2
Chris3606 <cridley106@gmail.com>
2021-10-22 21:17:39 -0500

    Added basic performance test framework and tests for ScreenObject main loop functions.

---
+++ PerformanceTests/SadConsole.PerformanceTests/Program.cs
@@ -0,0 +1,10 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace SadConsole.PerformanceTests
+{
+	class Program
+	{
+        private static void Main(string[] args)
+            => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+    }
+}
