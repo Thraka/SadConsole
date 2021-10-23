5aa46ea75350d601281b868f3152ba0e88b45097
Chris3606 <cridley106@gmail.com>
2021-10-22 21:17:39 -0500

    Added basic performance test framework and tests for ScreenObject main loop functions.

---
+++ PerformanceTests/SadConsole.PerformanceTests/BasicGameHost.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using SadConsole.Input;
+using SadConsole.Renderers;
+using SadRogue.Primitives;
+
+namespace SadConsole.PerformanceTests
+{
+    class BasicGameHost : GameHost
+    {
+        public class RenderStep : IRenderStep
+        {
+            public uint SortOrder { get => 1; set => throw new NotImplementedException(); }
+
+            public void Composing(IRenderer renderer, IScreenSurface screenObject) => throw new NotImplementedException();
+            public void Dispose() => throw new NotImplementedException();
+            public bool Refresh(IRenderer renderer, IScreenSurface screenObject, bool backingTextureChanged, bool isForced) => throw new NotImplementedException();
+            public void Render(IRenderer renderer, IScreenSurface screenObject) => throw new NotImplementedException();
+            public void Reset() => throw new NotImplementedException();
+            public void SetData(object data) { }
+        }
+
+        public class Texture : ITexture
+        {
+            private SixLabors.ImageSharp.Image _graphic;
+
+            public string ResourcePath { get; private set; }
+
+            public int Height => _graphic.Height;
+
+            public int Width => _graphic.Width;
+
+            public void Dispose()
+            {
+                _graphic.Dispose();
+            }
+            public Color GetPixel(Point position) => throw new NotImplementedException();
+            public Color GetPixel(int index) => throw new NotImplementedException();
+            public Color[] GetPixels() => throw new NotImplementedException();
+            public void SetPixel(Point position, Color color) => throw new NotImplementedException();
+            public void SetPixel(int index, Color color) => throw new NotImplementedException();
+            public ICellSurface ToSurface(TextureConvertMode mode, int surfaceWidth, int surfaceHeight, TextureConvertBackgroundStyle backgroundStyle = TextureConvertBackgroundStyle.Pixel, TextureConvertForegroundStyle foregroundStyle = TextureConvertForegroundStyle.Block, Color[] cachedColorArray = null, ICellSurface cachedSurface = null) => throw new NotImplementedException();
+
+            public Texture(string path)
+            {
+                using (Stream fontStream = new FileStream(path, FileMode.Open))
+                    _graphic = SixLabors.ImageSharp.Image.Load(fontStream);
+
+                ResourcePath = path;
+            }
+
+            public Texture(Stream textureStream)
+            {
+                _graphic = SixLabors.ImageSharp.Image.Load(textureStream);
+            }
+        }
+
+
+        public BasicGameHost()
+        {
+            Instance = this;
+            base.LoadDefaultFonts("");
+        }
+
+        public override IKeyboardState GetKeyboardState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IMouseState GetMouseState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IRenderer GetRenderer(string name)
+        {
+            return null;
+        }
+
+        public override ITexture GetTexture(string resourcePath)
+        {
+            return new Texture(resourcePath);
+        }
+
+        public override ITexture GetTexture(Stream textureStream)
+        {
+            return new Texture(textureStream);
+        }
+
+        public override IRenderStep GetRendererStep(string name)
+        {
+            return new RenderStep();
+        }
+
+        public override void ResizeWindow(int width, int height) => throw new NotImplementedException();
+
+        public override void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PerformanceTests/SadConsole.PerformanceTests/ScreenObject.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using SadConsole.Components;
+using SadConsole.Input;
+
+namespace SadConsole.PerformanceTests
+{
+    /// <summary>
+    /// Test component that does nothing except ensures it is processed in the Update/Render loops, for performance testing
+    /// </summary>
+    public class TestComponent : IComponent
+    {
+        public uint SortOrder { get; set; }
+
+        public void ProcessKeyboard(IScreenObject host, Keyboard keyboard, out bool handled) =>
+            handled = false;
+
+        public void ProcessMouse(IScreenObject host, MouseScreenObjectState state, out bool handled) =>
+            handled = false;
+
+        public void Update(IScreenObject host, TimeSpan delta)
+        { }
+
+        public void Render(IScreenObject host, TimeSpan delta)
+        { }
+
+        public virtual void OnAdded(IScreenObject host)
+        { }
+
+        public virtual void OnRemoved(IScreenObject host)
+        { }
+
+        uint IComponent.SortOrder => SortOrder;
+
+        bool IComponent.IsUpdate => true;
+
+        bool IComponent.IsRender => true;
+
+        bool IComponent.IsMouse => true;
+
+        bool IComponent.IsKeyboard => true;
+    }
+
+    /// <summary>
+    /// Series of benchmarks for various functionality of ScreenObject.  Benchmark functions that call methods which return void will instead return
+    /// the screen object being benchmarked, in an effort to ensure that the compiler will not optimize anything out.
+    /// </summary>
+    public class ScreenObject
+    {
+        #region Test Data
+
+        [Params(0, 1, 100, 1000)]
+        public int NumComponents;
+
+        [Params(0, 1, 100, 1000)]
+        public int NumChildren;
+
+        #endregion
+
+        private BasicGameHost _gameHost;
+        private SadConsole.ScreenObject _mainObject;
+        private TimeSpan _delta;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            _gameHost = new BasicGameHost();
+            _mainObject = new SadConsole.ScreenObject();
+            for (int i = 0; i < NumComponents; i++)
+                _mainObject.SadComponents.Add(new TestComponent());
+
+            for (int i = 0; i < NumChildren; i++)
+                _mainObject.Children.Add(new SadConsole.ScreenObject());
+
+            _delta = TimeSpan.FromMilliseconds(5);
+        }
+
+        #region Main Game Loop
+        [Benchmark]
+        public bool ProcessKeyboard()
+        {
+            return _mainObject.ProcessKeyboard(GameHost.Instance.Keyboard);
+        }
+
+        [Benchmark]
+        public bool ProcessMouse()
+        {
+            return _mainObject.ProcessMouse(new MouseScreenObjectState(_mainObject, GameHost.Instance.Mouse));
+        }
+
+        [Benchmark]
+        public SadConsole.ScreenObject Update()
+        {
+            _mainObject.Update(_delta);
+
+            return _mainObject;
+        }
+
+        [Benchmark]
+        public SadConsole.ScreenObject Render()
+        {
+            _mainObject.Render(_delta);
+
+            return _mainObject;
+        }
+        #endregion
+    }
+}
