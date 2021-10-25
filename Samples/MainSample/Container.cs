37939e08396184aba76e75479e99edb17f65f0a9
Ryszard Pyka <60892050+RychuP@users.noreply.github.com>
2021-10-25 05:48:44 +0100

    Added animated globe to the list of custom consoles in feature demo.

--- Samples/MainSample/Container.cs
+++ Samples/MainSample/Container.cs
@@ -45,7 +45,8 @@ namespace FeatureDemo
                 //new CustomConsole(new CustomConsoles.EntityZoneConsole(), "Entity zone demonstration", "Entity that can move in and out of zones."),
                 new CustomConsole(new CustomConsoles.BorderedConsole(), "Drawing borders around consoles", "Demonstration of using the Border class and a border component."),
                 new CustomConsole(new CustomConsoles.SubConsoleCursor(), "Subconsole Cursor", "Two consoles with a single backing TextSurface"),
-               
+                new CustomConsole(new CustomConsoles.AnimatedGlobe(), "Animated Globed", "Converting images and creating an animation with AnimatedScreenSurface."),
+                
                 //new CustomConsole(new CustomConsoles.ViewsAndSubViews(), "Sub Views", "Single text surface with two views into it. Click on either view."),
                 
                 //new CustomConsole(new CustomConsoles.EntityConsole(), "Game object", "Use the cursor keys to move the little character"),
