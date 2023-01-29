0026bd69a9ca9bb975cf621c0799bc0eaf28cb6d
Thraka <thraka.junk@outlook.com>
2023-01-28 20:53:36 -0800

    Update demo code to handle monogame specific rotated consoles

--- templates/template_code/SadConsole.Examples.Demo.CSharp/DemoRotated.MonoGame.cs
+++ templates/template_code/SadConsole.Examples.Demo.CSharp/DemoRotated.MonoGame.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿#if MONOGAME
+using Microsoft.Xna.Framework.Graphics;
 using SadConsole.DrawCalls;
 using SadConsole.Renderers;
 using Vector2 = Microsoft.Xna.Framework.Vector2;
@@ -13,7 +14,7 @@ internal class DemoRotatedSurface : IDemo
     public string Description => "This demo creates a custom rendering step and draw call which can rotate consoles.\r\n\r\n" +
                                  "As SadConsole renders objects, it builds a list of 'DrawCalls' that compose the console or surface. Such as, [c:r f:violet:3](1) clear the output texture [c:r f:violet:3](2) draw all the glyphs [c:r f:violet:3](3) apply tinting [c:r f:violet:3](4) render to the screen.\r\n";
 
-    public string CodeFile => "DemoShapes.cs";
+    public string CodeFile => "DemoShapes.MonoGame.cs";
 
     public IScreenSurface CreateDemoScreen() =>
         new RotatedSurface();
@@ -115,3 +116,4 @@ class RotatedOutputSurfaceRenderStep: IRenderStep
                                                0f);
     }
 }
+#endif
