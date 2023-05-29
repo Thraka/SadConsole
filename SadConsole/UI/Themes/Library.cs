c346eb5ea3188803866adf88be11f966f05619c7
Andy De George <2672110+Thraka@users.noreply.github.com>
2023-05-29 11:52:10 -0700

    Revert "Added tab control and associated themes (#317)" (#324)
    This reverts commit 1f101986e4b05d6f0dced99dcc334479d76ab72c.

--- SadConsole/UI/Themes/Library.cs
+++ SadConsole/UI/Themes/Library.cs
@@ -89,7 +89,6 @@ namespace SadConsole.UI.Themes
             SetControlTheme(typeof(Label), new LabelTheme());
             SetControlTheme(typeof(Panel), new PanelTheme());
             SetControlTheme(typeof(SurfaceViewer), new SurfaceViewerTheme());
-            SetControlTheme(typeof(TabControl), new HorizontalTabControlTheme());
         }
 
         /// <summary>
