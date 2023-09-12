22b585a6a49c6786256ab2b836304e00e41d5810
Thraka <thraka.junk@outlook.com>
2023-09-11 23:07:24 -0700

    Add docs to DecoratorHelpers.

--- SadConsole/DecoratorHelpers.cs
+++ SadConsole/DecoratorHelpers.cs
@@ -3,8 +3,16 @@ using System.Linq;
 
 namespace SadConsole;
 
+/// <summary>
+/// Helpers for <see cref="CellDecorator"/> and <see cref="ColoredGlyphBase.Decorators"/> which manages null on the property.
+/// </summary>
 public static class DecoratorHelpers
 {
+    /// <summary>
+    /// Replaces the decorators of a glyph.
+    /// </summary>
+    /// <param name="decorators">The decorators to set. <see langword="null"/> clears the decorators.</param>
+    /// <param name="glyph">The glyph to alter.</param>
     public static void SetDecorators(IEnumerable<CellDecorator>? decorators, ColoredGlyphBase glyph)
     {
         if (decorators is null)
@@ -19,6 +27,11 @@ public static class DecoratorHelpers
             glyph.Decorators = null;
     }
 
+    /// <summary>
+    /// Replaces the decorators of a glyph with a single decorator.
+    /// </summary>
+    /// <param name="decorator">The decorator to set.</param>
+    /// <param name="glyph">The glyph to alter.</param>
     public static void SetDecorator(CellDecorator decorator, ColoredGlyphBase glyph)
     {
         glyph.Decorators ??= new() { decorator };
@@ -27,6 +40,11 @@ public static class DecoratorHelpers
             glyph.Decorators = null;
     }
 
+    /// <summary>
+    /// Adds the decorators to a glyph.
+    /// </summary>
+    /// <param name="decorators">The decorators to add. Duplicates are skipped.</param>
+    /// <param name="glyph">The glyph to alter.</param>
     public static void AddDecorators(IEnumerable<CellDecorator>? decorators, ColoredGlyphBase glyph)
     {
         if (decorators is null) return;
@@ -41,6 +59,11 @@ public static class DecoratorHelpers
             glyph.Decorators = glyph.Decorators.Distinct().ToList();
     }
 
+    /// <summary>
+    /// Adds the specified decorator to a glyph.
+    /// </summary>
+    /// <param name="decorator">The decorator to add.</param>
+    /// <param name="glyph">The glyph to alter.</param>
     public static void AddDecorator(CellDecorator decorator, ColoredGlyphBase glyph)
     {
         glyph.Decorators ??= new List<CellDecorator>();
@@ -49,6 +72,11 @@ public static class DecoratorHelpers
             glyph.Decorators.Add(decorator);
     }
 
+    /// <summary>
+    /// Removes the specified decorators from a glyph. If no decorators remain on the glyph, the <see cref="ColoredGlyphBase.Decorators"/> collection is set to <see langword="null"/>.
+    /// </summary>
+    /// <param name="decorators">The decorators to remove.</param>
+    /// <param name="glyph">The glyph to alter.</param>
     public static void RemoveDecorators(IEnumerable<CellDecorator> decorators, ColoredGlyphBase glyph)
     {
         if (glyph.Decorators is null) return;
@@ -60,6 +88,11 @@ public static class DecoratorHelpers
             glyph.Decorators = null;
     }
 
+    /// <summary>
+    /// Removes the specified decorator from a glyph. If no decorators remain on the glyph, the <see cref="ColoredGlyphBase.Decorators"/> collection is set to <see langword="null"/>.
+    /// </summary>
+    /// <param name="decorator">The decorator to remove.</param>
+    /// <param name="glyph">The glyph to alter.</param>
     public static void RemoveDecorator(CellDecorator decorator, ColoredGlyphBase glyph)
     {
         if (glyph.Decorators is null) return;
@@ -70,6 +103,11 @@ public static class DecoratorHelpers
             glyph.Decorators = null;
     }
 
+    /// <summary>
+    /// Returns a new list of decorators from the <see cref="ColoredGlyphBase.Decorators"/> property of <paramref name="glyph"/>.
+    /// </summary>
+    /// <param name="glyph">The glyph to copy the decorators from.</param>
+    /// <returns>A list with all of the decorators from <paramref name="glyph"/>. If the glyph's decorators are <see langword="null"/>, <see langword="null"/> is returned.</returns>
     public static List<CellDecorator>? CloneDecorators(ColoredGlyphBase glyph)
     {
         if (glyph.Decorators == null) return null;
