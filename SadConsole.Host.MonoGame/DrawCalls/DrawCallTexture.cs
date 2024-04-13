d52e6391ade2335d9aa5b794663a1b204a3978d6
Thraka <thraka.junk@outlook.com>
2024-04-12 23:55:00 -0700

    Add shader effects to drawcalltexture in monogame

--- SadConsole.Host.MonoGame/DrawCalls/DrawCallTexture.cs
+++ SadConsole.Host.MonoGame/DrawCalls/DrawCallTexture.cs
@@ -14,6 +14,11 @@ public class DrawCallTexture : IDrawCall
     public Texture2D Texture;
 
     /// <summary>
+    /// The shader to use when drawing the texture.
+    /// </summary>
+    public Effect ShaderEffect;
+
+    /// <summary>
     /// Where on the <see cref="Host.Global.SharedSpriteBatch"/> to draw the texture.
     /// </summary>
     public Vector2 Position;
@@ -29,12 +34,11 @@ public class DrawCallTexture : IDrawCall
     /// <param name="texture">The image to draw.</param>
     /// <param name="position">The position on the <see cref="Host.Global.SharedSpriteBatch"/> to draw the image.</param>
     /// <param name="tint">A color tint to apply to the drawn image.</param>
+    /// <param name="effect">A shader to apply to the texture being drawn.</param>
     /// <exception cref="System.NullReferenceException">Thrown when <paramref name="texture"/> is null.</exception>
-    public DrawCallTexture(Texture2D texture, Vector2 position, Color? tint = null)
+    public DrawCallTexture(Texture2D texture, Vector2 position, Color? tint = null, Effect effect = null)
     {
-        if (texture == null) throw new System.NullReferenceException($"{nameof(texture)} cannot be null.");
-
-        Texture = texture;
+        Texture = texture ?? throw new System.NullReferenceException($"{nameof(texture)} cannot be null.");
         Position = position;
 
         if (tint.HasValue)
@@ -42,9 +46,26 @@ public class DrawCallTexture : IDrawCall
         else
             Tint = Color.White;
 
+        ShaderEffect = effect;
+
     }
 
     /// <inheritdoc/>
-    public void Draw() =>
-        Host.Global.SharedSpriteBatch.Draw(Texture, Position, Tint);
+    public void Draw()
+    {
+        if (ShaderEffect == null)
+        {
+            Host.Global.SharedSpriteBatch.Draw(Texture, Position, Tint);
+        }
+        else
+        {
+            DrawCallManager.InterruptBatch();
+
+            Host.Global.SharedSpriteBatch.Begin(SpriteSortMode.Deferred, Host.Settings.MonoGameScreenBlendState, SamplerState.PointClamp, DepthStencilState.DepthRead, RasterizerState.CullNone, ShaderEffect);
+            Host.Global.SharedSpriteBatch.Draw(Texture, Position, Tint);
+            Host.Global.SharedSpriteBatch.End();
+
+            DrawCallManager.ResumeBatch(true);
+        }
+    }
 }
