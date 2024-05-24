a89041d64593e8ab373dede1164f435b3d03d469
Thraka <thraka.junk@outlook.com>
2024-05-24 10:27:24 -0700

    Add Python example

---
+++ Samples/SadPython/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using Microsoft.Scripting.Hosting;
+using SadConsole.Components;
+using SadConsole.Input;
+using SadConsole.UI;
+using SadConsole.UI.Controls;
+using SadConsole.Configuration;
+
+Dictionary<string, object> options = new Dictionary<string, object>();
+options["Debug"] = true;
+
+ScriptEngine engine = IronPython.Hosting.Python.CreateEngine(options);
+Builder config;
+
+try
+{
+    ScriptSource script = engine.CreateScriptSourceFromFile("game.py");
+    CompiledCode compiledScript = script.Compile();
+
+
+    compiledScript.Execute();
+
+    config = compiledScript.DefaultScope.GetVariable("getGameConfig")();
+
+
+    Game.Create(config);
+    Game.Instance.Run();
+    Game.Instance.Dispose();
+}
+catch (Exception e1)
+{
+    System.Console.WriteLine(engine.GetService<ExceptionOperations>().FormatException(e1));
+}
+
+
+
+
+//Game.Configuration gameStartup = new Game.Configuration()
+//    .SetScreenSize(GameSettings.GAME_WIDTH, GameSettings.GAME_HEIGHT)
+//    .SetStartingScreen<multilayers>()
+//    .SetStartingScreen<othertest>()
+//    .SetStartingScreen<RootScene>()
+//    .IsStartingScreenFocused(false)
+//    .ConfigureFonts((f) => f.UseBuiltinFontExtended())
+//    ;
+
+//Game.Create(gameStartup);
+//Game.Instance.Run();
+//Game.Instance.Dispose();
+
---
+++ Samples/SadPython/game.py
@@ -0,0 +1,39 @@
+﻿import clr
+
+clr.AddReference("SadConsole")
+clr.AddReference("SadConsole.Host.MonoGame")
+
+from SadConsole import Game, CellSurfaceEditor as Edit, Settings as SadConsoleSettings
+from SadConsole import ScreenSurface, IScreenObject
+from SadConsole import Configuration;
+
+SadConsoleSettings.WindowTitle = "Python Game"
+
+def getGameConfig():
+    config = Configuration.Builder()
+    Configuration.Extensions.SetScreenSize(config, 20, 10)
+    #config.OnStart(Init)
+    Configuration.Extensions.SetStartingScreen(config, lambda host: MyConsole())
+    return config
+
+def Init():
+    Edit.FillWithRandomGarbage(Game.Instance.StartingConsole, Game.Instance.DefaultFont)
+
+
+class MyConsole(ScreenSurface):
+    def __new__(cls, *args):
+        if len(args) == 2:
+            instance = ScreenSurface.__new__(cls, args[0], args[1])
+
+        if len(args) == 0:
+            instance = ScreenSurface.__new__(cls, 20, 10)
+        else:
+            raise ValueError('I created this class to only support constructors of (Width, Height)')
+
+        Edit.FillWithRandomGarbage(instance, Game.Instance.DefaultFont)
+
+        return instance
+
+    def Update(self, delta):
+        Edit.Clear(self, 2, 2, 15)
+        Edit.Print(self, 2, 2, delta.ToString("G"))
