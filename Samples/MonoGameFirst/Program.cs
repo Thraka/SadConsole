399120f349db4c9b88c8bf7c7f36ebe0495aa46d
Thraka <thraka.junk@outlook.com>
2025-04-20 18:21:19 -0700

    Fix monogame-first sample

--- Samples/MonoGameFirst/Program.cs
+++ Samples/MonoGameFirst/Program.cs
@@ -2,13 +2,13 @@
 using SadConsole;
 using SadConsole.Configuration;
 using SadRogue.Primitives;
+using SadConsole.UI;
 
 // Configure how SadConsole starts up
 Builder startup = new Builder()
-    .SetScreenSize(90, 30)
+    .SetWindowSizeInCells(90, 30)
     .UseDefaultConsole()
     .OnStart(Game_Started)
-    .IsStartingScreenFocused(true)
     .ConfigureFonts(true)
     .SkipMonoGameGameCreation()
     ;
@@ -23,7 +23,12 @@ void Game_Started(object? sender, GameHost host)
     ColoredGlyph boxBorder = new(Color.White, Color.Black, 178);
     ColoredGlyph boxFill = new(Color.White, Color.Black);
 
-    Game.Instance.StartingConsole.FillWithRandomGarbage(255);
+    Game.Instance.StartingConsole.Surface.DefaultBackground = Color.Transparent;
+    Game.Instance.StartingConsole.Surface.Clear();
+
+    SadConsole.Settings.ClearColor = Color.Transparent;
+
+    //Game.Instance.StartingConsole.FillWithRandomGarbage(255);
     Game.Instance.StartingConsole.DrawBox(new Rectangle(2, 2, 26, 5), ShapeParameters.CreateFilled(boxBorder, boxFill));
     Game.Instance.StartingConsole.Print(4, 4, "Welcome to SadConsole!");
 }
