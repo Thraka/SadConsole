f65a282796365977c0296d933ee15d705ec83d9a
Thraka <thraka.junk@outlook.com>
2025-08-09 14:12:46 -0700

    Update templates for 10.6.0

--- templates/template_code/SadConsole.Examples.Demo.CSharp/Program.cs
+++ templates/template_code/SadConsole.Examples.Demo.CSharp/Program.cs
@@ -61,5 +61,4 @@ Builder
     .IsStartingScreenFocused(false) // Don't want RootScreen to be focused because RootScreen automatically focuses the selected demo console
     .SetWindowSizeInCells(GameSettings.GAME_WIDTH, GameSettings.GAME_HEIGHT)
     //.EnableImGuiDebugger(SadConsole.Input.Keys.F5)
-    .Run()
-    ;
+    .Run();
--- templates/template_code/SadConsole.Project.SFML.CSharp/Program.cs
+++ templates/template_code/SadConsole.Project.SFML.CSharp/Program.cs
@@ -2,13 +2,10 @@
 
 Settings.WindowTitle = "My SadConsole Game";
 
-Builder gameStartup = new Builder()
-    .SetScreenSize(GameSettings.GAME_WIDTH, GameSettings.GAME_HEIGHT)
+Builder
+    .GetBuilder()
+    .SetWindowSizeInCells(GameSettings.GAME_WIDTH, GameSettings.GAME_HEIGHT)
     .SetStartingScreen<SadConsoleGame.Scenes.RootScreen>()
     .IsStartingScreenFocused(true)
     .ConfigureFonts(true)
-    ;
-
-Game.Create(gameStartup);
-Game.Instance.Run();
-Game.Instance.Dispose();
+    .Run();
