85aa58538afb225f68b08e61435933345d40f855
The Vermilion Wizard <wizard@vermiliontower.com>
2025-09-25 09:01:48 -0700

    Initial commit

---
+++ GameSettings.cs
@@ -0,0 +1,5 @@
+﻿static class GameSettings
+{
+    public const int GAME_WIDTH = 90;
+    public const int GAME_HEIGHT = 30;    
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using SadConsole.Configuration;
+
+Settings.WindowTitle = "My SadConsole Game";
+
+Builder
+    .GetBuilder()
+    .SetWindowSizeInCells(GameSettings.GAME_WIDTH, GameSettings.GAME_HEIGHT)
+    .SetStartingScreen<SadConsoleFiddle.Scenes.RootScreen>()
+    .IsStartingScreenFocused(true)
+    .ConfigureFonts(true)
+    .Run();
