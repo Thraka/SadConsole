7e0800fea0f2c667f9dd7f7055e8f76430cb3f3d
Thraka <thraka.junk@outlook.com>
2025-11-01 14:48:12 -0700

    [editor]  Improve open file dialog

--- Samples/Editor/Windows/OpenFile.cs
+++ Samples/Editor/Windows/OpenFile.cs
@@ -9,23 +9,39 @@ public class OpenFile : ImGuiWindowBase
 {
     private FileListBox _fileListBox;
     private ImGuiList<IFileHandler>? _fileLoaders = new();
+    private bool _showDocumentTypes;
 
     public IFileHandler SelectedLoader;
     public FileInfo SelectedFile;
+
+    
+
+    public OpenFile(IEnumerable<IFileHandler> fileHandlers)
+    {
+        Title = "Open file";
+        _fileListBox = new(Directory.GetCurrentDirectory());
+        _fileLoaders = new ImGuiList<IFileHandler>(0, fileHandlers);
+        _showDocumentTypes = false;
+    }
 
     public OpenFile()
     {
         Title = "Open file";
         _fileListBox = new(Directory.GetCurrentDirectory());
+        _showDocumentTypes = true;
     }
-
     protected override void OnOpened()
     {
         base.OnOpened();
 
         // Reset to no document builders selected
         Core.State.DocumentBuilders.SelectedItemIndex = 0;
-        _fileLoaders = new ImGuiList<IFileHandler>(0, Core.State.DocumentBuilders.SelectedItem.GetLoadHandlers());
+
+        if (_showDocumentTypes)
+        {
+            Core.State.DocumentBuilders.SelectedItemIndex = 0;
+            _fileLoaders = new ImGuiList<IFileHandler>(0, Core.State.DocumentBuilders.SelectedItem.GetLoadHandlers());
+        }
     }
 
     public override void BuildUI(ImGuiRenderer renderer)
@@ -40,43 +56,43 @@ public class OpenFile : ImGuiWindowBase
 
             if (ImGui.BeginPopupModal(Title, ref IsOpen))
             {
-                ImGui.Text("Document Type:");
-                ImGui.SetNextItemWidth(ImGui.GetContentRegionAvail().X);
-
-                if (ImGui.ListBox("##doctypes",
-                                  ref Core.State.DocumentBuilders.SelectedItemIndex,
-                                  Core.State.DocumentBuilders.Names,
-                                  Core.State.DocumentBuilders.Count, 3))
+                if (_showDocumentTypes)
                 {
-                    _fileLoaders = new ImGuiList<IFileHandler>(0, Core.State.DocumentBuilders.SelectedItem.GetLoadHandlers());
+                    ImGui.Text("Document Type:");
+                    ImGui.SetNextItemWidth(ImGui.GetContentRegionAvail().X);
+
+                    if (ImGui.ListBox("##doctypes",
+                                      ref Core.State.DocumentBuilders.SelectedItemIndex,
+                                      Core.State.DocumentBuilders.Names,
+                                      Core.State.DocumentBuilders.Count, 3))
+                    {
+                        _fileLoaders = new ImGuiList<IFileHandler>(0, Core.State.DocumentBuilders.SelectedItem.GetLoadHandlers());
+                    }
+
+                    ImGui.Separator();
                 }
 
-                ImGui.Separator();
-
-                if (Core.State.DocumentBuilders.IsItemSelected())
+                if (ImGui.BeginTable("table1", 2, ImGuiTableFlags.BordersInnerV))
                 {
-                    if (ImGui.BeginTable("table1", 2, ImGuiTableFlags.BordersInnerV))
+                    ImGui.TableSetupColumn("one", ImGuiTableColumnFlags.WidthFixed, ImGui.GetContentRegionAvail().X / 3);
+                    ImGui.TableSetupColumn("two");
+
+                    // Draw left-side file handlers
+                    ImGui.TableNextRow();
+                    ImGui.TableSetColumnIndex(0);
+                    ImGui.AlignTextToFramePadding();
+                    ImGui.Text("File Types");
+                    ImGui.SetNextItemWidth(-1);
+                    ImGui.ListBox("##handlers", ref _fileLoaders.SelectedItemIndex, _fileLoaders.Names, _fileLoaders.Count, 10);
+
+                    // Draw right-side file list box;
+                    ImGui.TableSetColumnIndex(1);
+                    if (_fileLoaders.IsItemSelected())
                     {
-                        ImGui.TableSetupColumn("one", ImGuiTableColumnFlags.WidthFixed, ImGui.GetContentRegionAvail().X / 3);
-                        ImGui.TableSetupColumn("two");
-
-                        // Draw left-side file handlers
-                        ImGui.TableNextRow();
-                        ImGui.TableSetColumnIndex(0);
-                        ImGui.AlignTextToFramePadding();
-                        ImGui.Text("File Types");
-                        ImGui.SetNextItemWidth(-1);
-                        ImGui.ListBox("##handlers", ref _fileLoaders.SelectedItemIndex, _fileLoaders.Names, _fileLoaders.Count, 10);
-
-                        // Draw right-side file list box;
-                        ImGui.TableSetColumnIndex(1);
-                        if (_fileLoaders.IsItemSelected())
-                        {
-                            _fileListBox.SearchPattern = string.Join(';', _fileLoaders.SelectedItem.ExtensionsLoading.Select(ex => $"*.{ex}"));
-                            _fileListBox.Begin("listbox", new(-1, 300));
-                            _fileListBox.Draw();
-                            _fileListBox.End();
-                        }
+                        _fileListBox.SearchPattern = string.Join(';', _fileLoaders.SelectedItem.ExtensionsLoading.Select(ex => $"*.{ex}"));
+                        _fileListBox.Begin("listbox", new(-1, 300));
+                        _fileListBox.Draw();
+                        _fileListBox.End();
                     }
 
                     ImGui.EndTable();
