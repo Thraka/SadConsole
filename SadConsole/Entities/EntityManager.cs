46b24c6b7fd690c6f21b3bf594bce56ad53d7b9e
Thraka <thraka.junk@outlook.com>
2025-12-30 15:37:25 -0800

    Fix bug with entitymanager not hanlding animated surface visibility properly

--- SadConsole/Entities/EntityManager.cs
+++ SadConsole/Entities/EntityManager.cs
@@ -487,15 +487,15 @@ public class EntityManager : Components.UpdateComponent, Components.IComponent, 
                     ? _offsetAreaPixels.Intersects(GetAnimatedAreaOffsetByCenterPixel(entity))
                     : _screen!.Surface.View.Intersects(new Rectangle()
                                                         .WithPosition(entity.Position - entity.AppearanceSurface!.Animation.Center)
-                                                        .WithSize(entity.AppearanceSurface.Animation.ViewWidth * _screenCachedFontSize.X,
-                                                                  entity.AppearanceSurface.Animation.Height * _screenCachedFontSize.Y));
+                                                        .WithSize(entity.AppearanceSurface.Animation.Width,
+                                                                  entity.AppearanceSurface.Animation.Height));
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private Rectangle GetAnimatedAreaOffsetByCenterPixel(Entity entity) =>
         new(entity.Position.X - (entity.AppearanceSurface!.Animation.Center.X * _screenCachedFontSize.X),
                       entity.Position.Y - (entity.AppearanceSurface.Animation.Center.Y * _screenCachedFontSize.Y),
-                      entity.AppearanceSurface.Animation.ViewWidth * _screenCachedFontSize.X,
+                      entity.AppearanceSurface.Animation.Width * _screenCachedFontSize.X,
                       entity.AppearanceSurface.Animation.Height * _screenCachedFontSize.Y);
 
     private static int CompareEntity(Entity left, Entity right)
