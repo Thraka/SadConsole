48414b08f8fcd7113231d77e59fe7c1f0c624446
Thraka <thraka.junk@outlook.com>
2026-03-24 13:31:49 -0700

    Demo project dynamically loads unreferenced demos as "local" category

---
+++ Samples/Demo/Local/DemoFontManipulation.Other.cs
@@ -0,0 +1,26 @@
+﻿#if !MONOGAME
+namespace SadConsole.Examples;
+
+internal class DemoFontManipulation : IDemo
+{
+    public string Title => "Font manipulation";
+
+    public string Description => "This demo is only supported on MonoGame";
+
+    public string CodeFile => "DemoFontManipulation.Other.cs";
+
+    public IScreenSurface CreateDemoScreen() =>
+        new FontEditingScreen();
+
+    public override string ToString() =>
+        Title;
+}
+
+internal class FontEditingScreen : ScreenSurface
+{
+    public FontEditingScreen() : base(GameSettings.ScreenDemoBounds.Width, GameSettings.ScreenDemoBounds.Height / 2)
+    {
+        Surface.Print(2, 1, "Not supported in SFML, use a different host");
+    }
+}
+#endif
