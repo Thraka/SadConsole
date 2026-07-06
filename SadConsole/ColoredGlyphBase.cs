Thraka/SadConsole

commit 957d3bd77f761bdf34cea5736e79804eaad634cc

Fix bugs with string parser and decorators

SadConsole/ColoredGlyphBase.cs
@@ -117,7 +117,7 @@ public abstract partial class ColoredGlyphBase: IMatchable<ColoredGlyphBase>
         cell.Glyph = Glyph;
         cell.Mirror = Mirror;
         if (deepCopy)
-            cell.Decorators = Decorators != null ? new List<CellDecorator>(Decorators) : null;
+            cell.Decorators = CellDecoratorHelpers.CloneDecorators(cell);
         else
             cell.Decorators = Decorators;
     }
@@ -137,7 +137,7 @@ public abstract partial class ColoredGlyphBase: IMatchable<ColoredGlyphBase>
         Glyph = cell.Glyph;
         Mirror = cell.Mirror;
         if (deepCopy)
-            Decorators = cell.Decorators != null ? new List<CellDecorator>(cell.Decorators) : null;
+            cell.Decorators = CellDecoratorHelpers.CloneDecorators(cell);
         else
             Decorators = cell.Decorators;
     }

SadConsole/StringParser/ParseCommandDecorator.cs
@@ -83,9 +83,9 @@ public sealed class ParseCommandDecorator : ParseCommandBase
             Mirror = mirror;
             Glyph = glyph;
 
-            Color = Color.FromParser(paramArray[1], out _, out _, out _, out _, out _);
+            Color = Color.FromParser(paramArray[2], out _, out _, out _, out _, out _);
 
-            _counter = int.Parse(paramArray[2], CultureInfo.InvariantCulture);
+            _counter = int.Parse(paramArray[3], CultureInfo.InvariantCulture);
         }
         else
             throw badCommandException;
@@ -99,13 +99,10 @@ public sealed class ParseCommandDecorator : ParseCommandBase
 
         // Create decorator list if needed
         if (Replace)
-            glyphState.Decorators = new();
-        else
-            glyphState.Decorators ??= new();
+            CellDecoratorHelpers.RemoveAllDecorators(glyphState);
 
         // If decorator isn't already in the glyph state decorators, add it
-        if (glyphState.Decorators.IndexOf(Decorator) == -1)
-            glyphState.Decorators.Add(Decorator);
+        CellDecoratorHelpers.AddDecorator(Decorator, glyphState);
 
         // If counter is counting down
         if (_counter != -1)
@@ -116,9 +113,6 @@ public sealed class ParseCommandDecorator : ParseCommandBase
             if (_counter == 0)
             {
                 commandStack.RemoveSafe(this);
-
-                // Remove this decorator from the array
-                CellDecoratorHelpers.RemoveDecorator(Decorator, glyphState);
             }
         }
     }

commit a922a5054423b6cd55de1047659e7fe6b1a76c94

Fix bug introduced by 957d3bd77f761bdf34cea5736e79804eaad634cc (thx dad for spotting!)

SadConsole/ColoredGlyphBase.cs
@@ -137,7 +137,7 @@ public abstract partial class ColoredGlyphBase: IMatchable<ColoredGlyphBase>
         Glyph = cell.Glyph;
         Mirror = cell.Mirror;
         if (deepCopy)
-            cell.Decorators = CellDecoratorHelpers.CloneDecorators(cell);
+            Decorators = CellDecoratorHelpers.CloneDecorators(cell);
         else
             Decorators = cell.Decorators;
     }